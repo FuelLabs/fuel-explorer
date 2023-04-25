079be70b4ecdc13c950b88d0f6963d448916eea0
Braqzen <sydney.boroch@fuel.sh>
2023-04-25 16:59:37 +0100

    Updated schema

--- packages/indexer/src/lib.rs
+++ packages/indexer/src/lib.rs
@@ -5,73 +5,89 @@ use fuel_indexer_plugin::prelude::*;
 #[indexer(manifest = "indexer.manifest.yaml")]
 pub mod explorer_index {
     fn index_transaction(block_data: BlockData) {
-        // Logger::info(format!("{:?}", &block_data).as_str());
-
         for tx in block_data.transactions.iter() {
-
             let mut from: Option<&Address> = None;
             let mut to: Option<&Address> = None;
-            
+
             match &tx.transaction {
-                #[allow(unused)]
                 Transaction::Script(data) => {
-                    // Logger::info("Inside a script transaction. (>^‿^)>");
+                    Logger::info(
+                        format!(
+                            "(>^‿^)> Inside a script transaction {:?}.",
+                            block_data.height
+                        )
+                        .as_str(),
+                    );
+
                     for input in data.inputs() {
                         from = input.input_owner();
+                        if from.is_some() {
+                            break;
+                        }
                     }
                     for output in data.outputs() {
                         to = output.to();
+                        if to.is_some() {
+                            break;
+                        }
                     }
 
-                    let tx_entity = ScriptTx {
+                    TransactionEntity {
                         id: first8_bytes_to_u64(tx.id),
                         status: tx.status.clone().into(),
                         age: block_data.time,
-                        from: Bytes32::new(**from.unwrap()),
-                        to: Bytes32::new(**to.unwrap())
-                    };
-        
-                    tx_entity.save();
+                        input_address: Bytes32::new(**from.unwrap()),
+                        output_address: Bytes32::new(**to.unwrap()),
+                    }
+                    .save();
                 }
-                #[allow(unused)]
                 Transaction::Create(data) => {
-                    // Logger::info("Inside a create transaction. <(^.^)>");
+                    Logger::info(
+                        format!(
+                            "<(^.^)> Inside a create transaction {:?}.",
+                            block_data.height
+                        )
+                        .as_str(),
+                    );
 
                     for input in data.inputs() {
                         from = input.input_owner();
+                        if from.is_some() {
+                            break;
+                        }
                     }
                     for output in data.outputs() {
                         to = output.to();
+                        if to.is_some() {
+                            break;
+                        }
                     }
 
-                    let tx_entity = CreateTx {
+                    TransactionEntity {
                         id: first8_bytes_to_u64(tx.id),
                         status: tx.status.clone().into(),
                         age: block_data.time,
-                        from: Bytes32::new(**from.unwrap()),
-                        to: Bytes32::new(**to.unwrap())
-                    };
-        
-                    tx_entity.save();
+                        input_address: Bytes32::new(**from.unwrap()),
+                        output_address: Bytes32::new(**to.unwrap()),
+                    }
+                    .save();
                 }
-                #[allow(unused)]
                 Transaction::Mint(data) => {
-                    // Logger::info("Inside a mint transaction. <(^‿^<)");
-
-                    let tx_pointer = data.tx_pointer();
-                    let outputs = data.outputs();
+                    Logger::info(
+                        format!("<(^‿^<) Inside a mint transaction {:?}.", block_data.height)
+                            .as_str(),
+                    );
 
                     for output in data.outputs() {
                         to = output.to();
                     }
 
-                    let tx_entity = MintTx {
+                    MintTransactionEntity {
                         id: first8_bytes_to_u64(tx.id),
                         age: block_data.time,
-                        to: Bytes32::new(**to.unwrap())
-                    };
-
-                    tx_entity.save();
+                        output_address: Bytes32::new(**to.unwrap()),
+                    }
+                    .save();
                 }
             }
         }
