bc4e14d60738097ec0824beb4ae1db50211d9467
Braqzen <sydney.boroch@fuel.sh>
2023-05-02 21:51:18 +0100

    Unsupported item bug

--- packages/indexer/src/lib.rs
+++ packages/indexer/src/lib.rs
@@ -4,88 +4,71 @@ use fuel_indexer_plugin::prelude::*;
 
 #[indexer(manifest = "indexer.manifest.yaml")]
 pub mod explorer_index {
+    use serde_json::Value;
+
     fn index_transaction(block_data: BlockData) {
         for tx in block_data.transactions.iter() {
-            let mut from: Option<&Address> = None;
-            let mut to: Option<&Address> = None;
+            // let mut from: Option<&Address> = None;
+            // let mut to: Option<&Address> = None;
+
+            // Logger::info(format!("{:?}", &tx.transaction).as_str());
 
             match &tx.transaction {
                 Transaction::Script(data) => {
-                    Logger::info(
-                        format!(
-                            "(>^‿^)> Inside a script transaction {:?}.",
-                            block_data.height
-                        )
-                        .as_str(),
-                    );
+                    // Logger::info(
+                    //     format!(
+                    //         "(>^‿^)> Inside a script transaction {:?}.",
+                    //         block_data.height
+                    //     )
+                    //     .as_str(),
+                    // );
 
-                    for input in data.inputs() {
-                        from = input.input_owner();
-                        if from.is_some() {
-                            break;
-                        }
-                    }
-                    for output in data.outputs() {
-                        to = output.to();
-                        if to.is_some() {
-                            break;
-                        }
-                    }
+                    // let output: Value = serde_json::from_str(t.outputs());
+                    let inputs: Value = serde_json::from_str(data.inputs());
+                    // let status: Value = serde_json::from_str(tx.status.clone());
 
                     TransactionEntity {
                         id: first8_bytes_to_u64(tx.id),
-                        status: tx.status.clone().into(),
+                        // status: Some(status),
                         age: block_data.time,
-                        input_address: Bytes32::new(**from.unwrap()),
-                        output_address: Bytes32::new(**to.unwrap()),
+                        inputs: Some(inputs),
+                        outputs: Json(serde_json::to_value(data.outputs()).unwrap().to_string()),
                     }
                     .save();
                 }
                 Transaction::Create(data) => {
-                    Logger::info(
-                        format!(
-                            "<(^.^)> Inside a create transaction {:?}.",
-                            block_data.height
-                        )
-                        .as_str(),
-                    );
-
-                    for input in data.inputs() {
-                        from = input.input_owner();
-                        if from.is_some() {
-                            break;
-                        }
-                    }
-                    for output in data.outputs() {
-                        to = output.to();
-                        if to.is_some() {
-                            break;
-                        }
-                    }
+                    // Logger::info(
+                    //     format!(
+                    //         "<(^.^)> Inside a create transaction {:?}.",
+                    //         block_data.height
+                    //     )
+                    //     .as_str(),
+                    // );
+                    
+                    let inputs: Value = serde_json::from_str(data.inputs());
+                    // let status: Value = serde_json::from_str(tx.status.clone());
 
                     TransactionEntity {
                         id: first8_bytes_to_u64(tx.id),
-                        status: tx.status.clone().into(),
+                        // status: Some(status),
                         age: block_data.time,
-                        input_address: Bytes32::new(**from.unwrap()),
-                        output_address: Bytes32::new(**to.unwrap()),
+                        inputs: Some(inputs),
+                        outputs: Json(serde_json::to_value(data.outputs()).unwrap().to_string()),
                     }
                     .save();
                 }
                 Transaction::Mint(data) => {
-                    Logger::info(
-                        format!("<(^‿^<) Inside a mint transaction {:?}.", block_data.height)
-                            .as_str(),
-                    );
+                    // Logger::info(
+                    //     format!("<(^‿^<) Inside a mint transaction {:?}.", block_data.height)
+                    //         .as_str(),
+                    // );
 
-                    for output in data.outputs() {
-                        to = output.to();
-                    }
-
-                    MintTransactionEntity {
+                    TransactionEntity {
                         id: first8_bytes_to_u64(tx.id),
+                        // status: None,
                         age: block_data.time,
-                        output_address: Bytes32::new(**to.unwrap()),
+                        inputs: None,
+                        outputs: Json(serde_json::to_value(data.outputs()).unwrap().to_string()),
                     }
                     .save();
                 }
