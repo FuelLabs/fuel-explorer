10086f80c492cc80e7aa6a2cb882138d9211b689
Braqzen <sydney.boroch@fuel.sh>
2023-05-04 17:36:35 +0100

    Simplified the block

--- packages/indexer/src/lib.rs
+++ packages/indexer/src/lib.rs
@@ -5,33 +5,14 @@ use fuel_indexer_plugin::prelude::*;
 #[indexer(manifest = "indexer.manifest.yaml")]
 pub mod explorer_index {
     fn index_block(block_data: BlockData) {
-        let mut header = Header {
-            id: first8_bytes_to_u64(block_data.id),
-            transaction_count: 0,
-            output_message_count: 0,
-            transactions_root: Bytes32::zeroed(),
-            height: block_data.height,
-            previous_root: Bytes32::zeroed(),
-            timestamp: block_data.time,
-            application_hash: Bytes32::zeroed(),
-        };
-
-        for tx in block_data.transactions.iter() {
-            header.transaction_count += 1;
-
-            for receipt in &tx.receipts {
-                match receipt {
-                    Receipt::MessageOut { .. } => {
-                        header.output_message_count += 1;
-                    }
-                    _ => (),
-                }
-            }
-        }
+        // TODO: fetch Tx from block and index
 
         Block {
             id: first8_bytes_to_u64(block_data.id),
-            header,
+            previous_root: Bytes32::zeroed(),
+            height: block_data.height,
+            timestamp: block_data.time,
+            // transactions: vec![],
         }
         .save();
     }
