419197f8f90bc802135296de55ac6bbba2500008
Luiz Estácio | stacio.eth <luizstacio@gmail.com>
2023-08-29 19:59:27 +0100

    feat: add preview deployment (#29)

--- packages/app/.storybook/main.ts
+++ packages/app/.storybook/main.ts
@@ -1,9 +1,11 @@
 import tsconfigpath from 'vite-tsconfig-paths';
 import { mergeConfig } from 'vite';
 import type { StorybookConfig } from '@storybook/react-vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { join } from 'path';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.tsx'],
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.tsx'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
@@ -19,8 +21,21 @@ const config: StorybookConfig = {
     options: {},
   },
   async viteFinal(config: any) {
+    console.log(config.base);
     return mergeConfig(config, {
-      plugins: [tsconfigpath()],
+      base: process.env.NODE_ENV === 'development' ? config.base : '/storybook',
+      plugins: [
+        tsconfigpath(),
+        viteStaticCopy({
+          targets: [
+            {
+              src: config.build.outDir,
+              dest: join(__dirname, '../public'),
+              rename: 'storybook',
+            },
+          ],
+        }),
+      ],
       define: {
         __STORYBOOK_FUEL_UI__: '"true"',
       },
