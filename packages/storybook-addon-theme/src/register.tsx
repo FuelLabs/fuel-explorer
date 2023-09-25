e989164cab84b89543618ee60c6647660244600a
Pedro Nauck <pedronauck@gmail.com>
2023-09-25 12:30:59 -0300

    fix: eslint warnings

--- packages/storybook-addon-theme/src/register.tsx
+++ packages/storybook-addon-theme/src/register.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { addons, types } from '@storybook/addons';
 import { IconButton, Icons } from '@storybook/components';
 import { FORCE_RE_RENDER } from '@storybook/core-events';
