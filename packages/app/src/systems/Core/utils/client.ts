3537745d79222b0673611f57bcc3f9b5c1243169
Pedro Nauck <pedronauck@gmail.com>
2023-09-26 15:45:06 -0300

    fix: vercel url when prod

--- packages/app/src/systems/Core/utils/client.ts
+++ packages/app/src/systems/Core/utils/client.ts
@@ -1,5 +1,12 @@
 import { GraphQLClient } from 'graphql-request';
+import { resolve } from 'url';
 import { getSdk } from '~/graphql/generated/types';
 
-const graphql = new GraphQLClient('http://localhost:3000/api/graphql');
+const VERCEL_URL = process.env.VERCEL_URL;
+const API_URL =
+  process.env.NODE_ENV === 'production'
+    ? resolve(VERCEL_URL!, '/api/graphql')
+    : 'http://localhost:3000/api/graphql';
+
+const graphql = new GraphQLClient(API_URL);
 export const client = getSdk(graphql);
