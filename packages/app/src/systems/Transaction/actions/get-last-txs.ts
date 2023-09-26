a1fed6a05f04a4d6ed30be1976c300c9e89d8c75
Luiz Estácio <luizstacio@gmail.com>
2023-09-25 23:04:46 -0300

    feat: add catch to avoid error on build time

--- packages/app/src/systems/Transaction/actions/get-last-txs.ts
+++ packages/app/src/systems/Transaction/actions/get-last-txs.ts
@@ -1,16 +1,16 @@
 'use server';
-import { getSdk } from '@fuel-explorer/graphql';
-import { GraphQLClient } from 'graphql-request';
+
 import { z } from 'zod';
 import { act } from '~/systems/Core/utils/act-server';
+import { explorer } from '~/systems/explerer';
 
 const schema = z.object({
   last: z.number().default(12).optional(),
 });
 
 export const getLastTxs = act(schema, async ({ last = 12 }) => {
-  const client = new GraphQLClient(process.env.GRAPHQL_API!);
-  const sdk = getSdk(client);
-  const { data } = await sdk.getLastTransactions({ last });
+  const { data } = await explorer.getLastTransactions({ last }).catch(() => ({
+    data: { transactions: { nodes: [] } },
+  }));
   return data.transactions.nodes;
 });
