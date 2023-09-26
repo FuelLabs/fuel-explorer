d0061ecf4fa6b3ec60eb9a3558d99958cbf7ff6f
Pedro Nauck <pedronauck@gmail.com>
2023-09-26 16:04:50 -0300

    fix: vercel env

--- packages/app/src/systems/Core/utils/client.ts
+++ packages/app/src/systems/Core/utils/client.ts
@@ -2,10 +2,11 @@ import { GraphQLClient } from 'graphql-request';
 import { resolve } from 'url';
 import { getSdk } from '~/graphql/generated/types';
 
-const VERCEL_URL = process.env.VERCEL_URL;
-const API_URL = process.env.VERCEL_URL
-  ? resolve(VERCEL_URL!, '/api/graphql')
-  : 'http://localhost:3000/api/graphql';
+const getBaseUrl = () => {
+  if (process.env.VERCEL_ENV) return `https://${process.env.VERCEL_URL}`;
+  return 'http://localhost:3000';
+};
 
+const API_URL = resolve(getBaseUrl(), '/api/graphql');
 const graphql = new GraphQLClient(API_URL);
 export const client = getSdk(graphql);
