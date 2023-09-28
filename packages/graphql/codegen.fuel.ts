5941cac80142381202d6e15d4a067c36ed1e4962
Pedro Nauck <pedronauck@gmail.com>
2023-09-27 22:44:28 -0300

    fix: build

--- packages/graphql/codegen.fuel.ts
+++ packages/graphql/codegen.fuel.ts
@@ -3,7 +3,8 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
   generates: {
     './src/schemas/fuelcore.graphql': {
-      schema: process.env.FUEL_PROVIDER_URL,
+      schema:
+        process.env.FUEL_PROVIDER_URL || 'http://beta-3.fuel.network/graphql',
       plugins: ['schema-ast'],
       config: {
         includeDirectives: true,
