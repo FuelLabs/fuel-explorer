a32a8ec69d86e7d7005a741ff8b52d3c64a19b3c
Pedro Nauck <pedronauck@gmail.com>
2023-10-27 17:00:02 -0300

    chore: page title outside layout

--- packages/app/src/app/account/[id]/layout.tsx
+++ packages/app/src/app/account/[id]/layout.tsx
@@ -1,23 +1,20 @@
-import { Address, Box, VStack } from '@fuels/ui';
-import { IconHash } from '@tabler/icons-react';
+import { Box, VStack } from '@fuels/ui';
 import { AccountTabs } from '~/systems/Account/components/AccountTabs/AccountTabs';
 import { Layout } from '~/systems/Core/components/Layout/Layout';
-import { PageTitle } from '~/systems/Core/components/PageTitle/PageTitle';
 
 export default function AccountLayout({
   children,
+  title,
   params: { id },
 }: {
+  title: React.ReactNode;
   children: React.ReactNode;
   params: { id: string };
 }) {
   return (
     <Layout>
       <VStack>
-        <PageTitle icon={<IconHash size={20} stroke={1.2} />}>
-          Account
-          <Address full value={id} />
-        </PageTitle>
+        {title}
         <AccountTabs accountId={id} />
         <Box as="section" className="mt-10">
           {children}
