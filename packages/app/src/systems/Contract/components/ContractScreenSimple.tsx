32201b31553a1bcecb750f550633ba50bbf8a5a9
LuizAsFight <felipebolsonigomes@gmail.com>
2023-10-27 18:25:25 -0300

    feat: change page structure (4 tabs)

--- packages/app/src/systems/Contract/components/ContractScreenSimple.tsx
+++ packages/app/src/systems/Contract/components/ContractScreenSimple.tsx
@@ -1,22 +1,18 @@
 import type { ContractItemFragment } from '@fuel-explorer/graphql';
-import {
-  Box,
-  Copyable,
-  Flex,
-  Tabs,
-  Text,
-  VStack,
-  useFuelAddress,
-} from '@fuels/ui';
+import { Box, Copyable, Flex, Tabs, VStack, useFuelAddress } from '@fuels/ui';
 import {
   IconChecklist,
   IconCodeAsterix,
   IconCoins,
   IconSquareRoundedPlus,
 } from '@tabler/icons-react';
+import { usePathname, useRouter } from 'next/navigation';
 import { CardInfo } from '~/systems/Core/components/CardInfo/CardInfo';
 
+import { TabAssets } from './TabAssets';
+import { TabMinted } from './TabMinted';
 import { TabSource } from './TabSource';
+import { TabTransactions } from './TabTransactions';
 
 type ContractScreenProps = {
   contract: ContractItemFragment;
@@ -24,6 +20,10 @@ type ContractScreenProps = {
 
 export function ContractScreenSimple({ contract }: ContractScreenProps) {
   const { short, address } = useFuelAddress(contract.id);
+  const router = useRouter();
+  const pathname = usePathname();
+  const tabPathname = pathname?.split('/').slice(-1)[0];
+
   return (
     <VStack>
       <Flex gap="4">
@@ -31,7 +31,12 @@ export function ContractScreenSimple({ contract }: ContractScreenProps) {
           <Copyable value={address}>{short}</Copyable>
         </CardInfo>
       </Flex>
-      <Tabs defaultValue="source">
+      <Tabs
+        defaultValue={tabPathname || ''}
+        onValueChange={(tabChoosed) =>
+          router.push(`/contract/${contract.id}/${tabChoosed}`)
+        }
+      >
         <Tabs.List>
           <Tabs.Trigger value="transactions">
             <IconChecklist size={15} className="mr-1" />
@@ -51,17 +56,13 @@ export function ContractScreenSimple({ contract }: ContractScreenProps) {
           </Tabs.Trigger>
         </Tabs.List>
         <Box className="pt-3 pb-2">
-          <Tabs.Content value="transactions">
-            <Text size="2">Transactions.</Text>
-          </Tabs.Content>
-          <Tabs.Content value="assets">
-            <Text size="2">Assets.</Text>
-          </Tabs.Content>
-          <Tabs.Content value="minted">
-            <Text size="2">Minted.</Text>
-          </Tabs.Content>
-          <Tabs.Content value="source">
-            <TabSource bytecode={contract.bytecode} />
+          <Tabs.Content value={tabPathname || ''}>
+            {tabPathname === 'transactions' && <TabTransactions />}
+            {tabPathname === 'assets' && <TabAssets />}
+            {tabPathname === 'minted' && <TabMinted />}
+            {tabPathname === 'source' && (
+              <TabSource bytecode={contract.bytecode} />
+            )}
           </Tabs.Content>
         </Box>
       </Tabs>
