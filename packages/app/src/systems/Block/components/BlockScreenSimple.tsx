91cd6011c108afcdd7bdfd9c153c7e5772fede7c
Matt Auer <mattauer@umich.edu>
2023-11-02 14:40:03 -0500

    feat: show more info on the block page

--- packages/app/src/systems/Block/components/BlockScreenSimple.tsx
+++ packages/app/src/systems/Block/components/BlockScreenSimple.tsx
@@ -1,5 +1,16 @@
 import type { BlockItemFragment, Maybe } from '@fuel-explorer/graphql';
-import { Flex, HStack, VStack } from '@fuels/ui';
+import {
+  Card,
+  Flex,
+  HStack,
+  VStack,
+  Text,
+  shortAddress,
+  Link,
+} from '@fuels/ui';
+import { IconChecklist } from '@tabler/icons-react';
+import { bn } from 'fuels';
+import NextLink from 'next/link';
 import { CardInfo } from '~/systems/Core/components/CardInfo/CardInfo';
 
 type BlockScreenSimpleProps = {
@@ -7,12 +18,9 @@ type BlockScreenSimpleProps = {
 };
 
 export function BlockScreenSimple({ block }: BlockScreenSimpleProps) {
-  console.log('temp: ', block?.header.time);
-  console.log('here: ', block?.time);
-
   return (
     <VStack>
-      <HStack>
+      <HStack className="my-6">
         <CardInfo name="Producer" className="flex-1">
           TODO
         </CardInfo>
@@ -24,14 +32,45 @@ export function BlockScreenSimple({ block }: BlockScreenSimpleProps) {
           {block?.time?.fromNow}
         </CardInfo>
         <CardInfo name="Gas spent (gwei)" className="flex-1">
-          TODO
+          {bn(block?.totalGasUsed).format()}
         </CardInfo>
         <CardInfo name="# of transactions" className="flex-1">
           {block?.header.transactionsCount}
         </CardInfo>
       </HStack>
-      <Flex>transaction info header</Flex>
-      <Flex>Transactions</Flex>
+      <Flex className="border-b border-border pb-4">
+        <Card>
+          <Card.Body>
+            <Flex>
+              <IconChecklist /> Transactions
+            </Flex>
+          </Card.Body>
+        </Card>
+      </Flex>
+      <Flex>
+        {block?.transactions.map((transaction) => {
+          return (
+            <Card key={transaction.id} className="flex-1">
+              <Card.Header className="justify-between flex-row">
+                <HStack>
+                  <Text>{transaction.title}</Text>
+                  <Link as={NextLink} href={`/tx/${transaction.id}`}>
+                    <Text className="text-black">
+                      {shortAddress(transaction.id)}
+                    </Text>
+                  </Link>
+                </HStack>
+                <HStack>
+                  <Text>{}</Text>
+                  <Text className="text-muted">
+                    {bn(transaction.gasUsed).format()} gwei
+                  </Text>
+                </HStack>
+              </Card.Header>
+            </Card>
+          );
+        })}
+      </Flex>
     </VStack>
   );
 }
