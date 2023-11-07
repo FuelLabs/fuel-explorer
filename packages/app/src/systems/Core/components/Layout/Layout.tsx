9dde0c86a151464ce2be3d5d14d8a516acc55428
Matt Auer <mattauer@umich.edu>
2023-11-07 10:17:30 -0600

    feat: add mobile

--- packages/app/src/systems/Core/components/Layout/Layout.tsx
+++ packages/app/src/systems/Core/components/Layout/Layout.tsx
@@ -52,6 +52,42 @@ export function Layout({ children, hero }: LayoutProps) {
           </Nav.Menu>
           <Nav.ThemeToggle onToggle={(theme) => setTheme({ theme })} />
         </Nav.Desktop>
+        <Nav.Mobile>
+          <Nav.MobileContent className="align-center">
+            <NextLink href="/" className="flex-1 items-center">
+              <Nav.Logo className="justify-center align-center" />
+            </NextLink>
+            <Nav.ThemeToggle />
+          </Nav.MobileContent>
+          <Nav.Menu>
+            <Nav.MenuItem href="https://docs.fuel.network/">
+              Developers
+            </Nav.MenuItem>
+            <Nav.MenuItem href="https://forum.fuel.network">
+              Community
+            </Nav.MenuItem>
+            <Nav.MenuItem isExternal href="https://fuel.network">
+              Labs
+            </Nav.MenuItem>
+            <Nav.MenuItem
+              isExternal
+              href="https://alpha.fuel.network/bridge"
+              target="_blank"
+            >
+              Bridge
+            </Nav.MenuItem>
+            <Nav.MenuItem isActive as={NextLink} href="/">
+              Explorer
+            </Nav.MenuItem>
+            <Nav.MenuItem
+              isExternal
+              href="https://alpha.fuel.network/ecosystem"
+              target="_blank"
+            >
+              Ecosystem
+            </Nav.MenuItem>
+          </Nav.Menu>
+        </Nav.Mobile>
       </Nav>
       {hero && <Hero />}
       <Container className="py-16 pb-20" size="4">
