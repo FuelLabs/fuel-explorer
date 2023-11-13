bea12ccca840682e4e93724b35434d3227bb4a5b
Matt <54373384+matt-user@users.noreply.github.com>
2023-11-13 15:23:31 -0600

    refactor: move pagination component (#111)
    closes FE-16

--- packages/app/src/app/transactions/[page]/layout.tsx
+++ packages/app/src/app/transactions/[page]/layout.tsx
@@ -4,18 +4,16 @@ import { Layout } from '~/systems/Core/components/Layout/Layout';
 import { TransactionsTitle } from './title';
 
 export default async function LayoutTransactions({
-  params,
   children,
 }: {
   children: ReactNode;
-  params: { page: string };
 }) {
   return (
     <Layout
       hero
       contentClassName="[&_.rt-ContainerInner]:space-y-2 tablet:[&_.rt-ContainerInner]:space-y-2 laptop:[&_.rt-ContainerInner]:space-y-10"
     >
-      <TransactionsTitle page={params.page} />
+      <TransactionsTitle />
       {children}
     </Layout>
   );
