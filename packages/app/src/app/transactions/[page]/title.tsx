05562cc1a6f8ecd51c782fc0ccd6cea893ae7005
Matt Auer <mattauer@umich.edu>
2023-11-13 13:36:08 -0600

    refactor: move pagination component

--- packages/app/src/app/transactions/[page]/title.tsx
+++ packages/app/src/app/transactions/[page]/title.tsx
@@ -1,32 +1,11 @@
 'use client';
 import { Icon } from '@fuels/ui';
 import { IconListDetails } from '@tabler/icons-react';
-import { useRouter } from 'next/navigation';
-import { useWindowSize } from 'react-use';
 import { PageTitle } from '~/systems/Core/components/PageTitle/PageTitle';
-import { Pagination } from '~/systems/Core/components/Pagination/Pagination';
 
-type ComponentProps = {
-  page: string;
-};
-
-export function TransactionsTitle({ page: current = '1' }: ComponentProps) {
-  const page = Number(current);
-  const { width } = useWindowSize();
-  const router = useRouter();
-
+export function TransactionsTitle() {
   return (
-    <PageTitle
-      icon={<Icon icon={IconListDetails} />}
-      rightElement={
-        width >= 640 && (
-          <Pagination
-            page={page}
-            onChange={(page) => router.push(`/transactions/${page}`)}
-          />
-        )
-      }
-    >
+    <PageTitle icon={<Icon icon={IconListDetails} />}>
       Recent Transactions
     </PageTitle>
   );
