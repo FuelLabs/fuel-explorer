ee453f0f2abc9d800e03c5fd33b192c4704d10b0
Pedro Nauck <pedronauck@gmail.com>
2023-11-14 16:28:19 -0300

    chore: add new variant on Tabs component

--- packages/ui/src/components/Tabs/Tabs.tsx
+++ packages/ui/src/components/Tabs/Tabs.tsx
@@ -1,34 +1,76 @@
-import { Tabs as RT } from '@radix-ui/themes';
+import * as RT from '@radix-ui/react-tabs';
+import { createContext, useContext } from 'react';
+import { useIconProps } from 'src/hooks/useIconProps';
+import type { WithIconProps } from 'src/hooks/useIconProps';
+import type { VariantProps } from 'tailwind-variants';
+import { tv } from 'tailwind-variants';
 
 import { createComponent, withNamespace } from '../../utils/component';
 import type { PropsOf } from '../../utils/types';
 
-export type TabsProps = PropsOf<typeof RT.Root> & { defaultValue: string };
+export type TabsVariantProps = VariantProps<typeof styles>;
+export type TabsProps = PropsOf<typeof RT.Root> &
+  TabsVariantProps & {
+    defaultValue: string;
+  };
+
 export type TabsListProps = PropsOf<typeof RT.List>;
-export type TabsTriggerProps = PropsOf<typeof RT.Trigger> & { value: string };
+export type TabsTriggerProps = PropsOf<typeof RT.Trigger> &
+  WithIconProps & {
+    value: string;
+  };
+
 export type TabsContentProps = PropsOf<typeof RT.Content> & { value: string };
+
+const ctx = createContext<TabsVariantProps>({} as TabsVariantProps);
 
 export const TabsRoot = createComponent<TabsProps, typeof RT.Root>({
   id: 'Tabs',
   baseElement: RT.Root,
+  render: (
+    Root,
+    { children, className, size = '1', variant = 'surface', ...props },
+  ) => {
+    const classes = styles({ size, variant });
+    return (
+      <ctx.Provider value={{ size, variant }}>
+        <Root className={classes.root({ className })} {...props}>
+          {children}
+        </Root>
+      </ctx.Provider>
+    );
+  },
 });
 
 export const TabsList = createComponent<TabsListProps, typeof RT.List>({
   id: 'TabsList',
   baseElement: RT.List,
+  className: () => {
+    const { size, variant } = useContext(ctx);
+    return styles({ size, variant }).list();
+  },
 });
 
-export const TabsTrigger = createComponent<TabsTriggerProps, typeof RT.Trigger>(
-  {
-    id: 'TabsTrigger',
-    baseElement: RT.Trigger,
+export const TabsTrigger = createComponent<TabsTriggerProps, 'button'>({
+  id: 'TabsTrigger',
+  render: (_, { className, ...props }) => {
+    const { size, variant } = useContext(ctx);
+    const classes = styles({ size, variant });
+    const itemProps = useIconProps({ size, ...props });
+    return (
+      <RT.Trigger className={classes.trigger({ className })} {...itemProps} />
+    );
   },
-);
+});
 
 export const TabsContent = createComponent<TabsContentProps, typeof RT.Content>(
   {
     id: 'TabsContent',
     baseElement: RT.Content,
+    className: () => {
+      const { size, variant } = useContext(ctx);
+      return styles({ size, variant }).content();
+    },
   },
 );
 
@@ -36,4 +78,98 @@ export const Tabs = withNamespace(TabsRoot, {
   List: TabsList,
   Trigger: TabsTrigger,
   Content: TabsContent,
+});
+
+const styles = tv({
+  slots: {
+    root: ['flex flex-col gap-4'],
+    list: ['shrink-0 flex'],
+    trigger: ['flex items-center justify-center'],
+    content: ['grow'],
+  },
+  variants: {
+    variant: {
+      surface: {
+        list: ['gap-4'],
+        trigger: [
+          'bg-transparent text-muted rounded border border-border',
+          'enabled:hover:bg-gray-2 enabled:hover:text-heading transition-colors',
+          'aria-[selected=true]:bg-gray-2 aria-[selected=true]:text-heading',
+          'fuel-[Icon]:hover:text-icon',
+          'fuel-[Icon]:aria-[selected=true]:text-icon',
+          'disabled:opacity-50',
+        ],
+      },
+      line: {
+        list: ['gap-4 border-b border-border'],
+        trigger: [
+          'bg-transparent text-muted',
+          'enabled:hover:text-heading transition-colors',
+          'aria-[selected=true]:text-heading aria-[selected=true]:border-b aria-[selected=true]:border-accent',
+          'fuel-[Icon]:hover:text-icon',
+          'fuel-[Icon]:aria-[selected=true]:text-icon',
+          'disabled:opacity-50',
+        ],
+      },
+    },
+    size: {
+      '1': {
+        trigger: 'gap-2',
+      },
+      '2': {
+        trigger: 'gap-3',
+      },
+      '3': {
+        trigger: 'gap-4',
+      },
+    },
+  },
+  compoundVariants: [
+    {
+      variant: 'surface',
+      size: '1',
+      class: {
+        trigger: ['h-8 px-3 text-sm'],
+      },
+    },
+    {
+      variant: 'surface',
+      size: '2',
+      class: {
+        trigger: ['h-10 px-4 text-md'],
+      },
+    },
+    {
+      variant: 'surface',
+      size: '3',
+      class: {
+        trigger: ['h-12 px-5 text-lg'],
+      },
+    },
+    {
+      variant: 'line',
+      size: '1',
+      class: {
+        trigger: ['py-2 px-3 text-sm'],
+      },
+    },
+    {
+      variant: 'line',
+      size: '2',
+      class: {
+        trigger: ['py-3 px-4 text-md'],
+      },
+    },
+    {
+      variant: 'line',
+      size: '3',
+      class: {
+        trigger: ['py-4 px-5 text-lg'],
+      },
+    },
+  ],
+  defaultVariants: {
+    variant: 'surface',
+    size: '1',
+  },
 });
