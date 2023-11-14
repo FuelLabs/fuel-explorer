740b34397e167593f6fcb20f1bd4cf92910c5e18
Pedro Nauck <pedronauck@gmail.com>
2023-11-13 21:09:29 -0300

    fix: adjust pages inside nextjs app

--- packages/app/src/app/transactions/page.tsx
+++ packages/app/src/app/transactions/page.tsx
@@ -1,17 +1,15 @@
-import { redirect } from 'next/navigation';
 import { getLastTxs } from '~/systems/Transaction/actions/get-last-txs';
 import { TxList } from '~/systems/Transaction/component/TxList/TxList';
 
 type PageProps = {
-  params: {
-    page: string;
+  searchParams: {
+    page?: string;
   };
 };
 
 export default async function Transactions({
-  params: { page = '1' },
+  searchParams: { page = '1' },
 }: PageProps) {
-  if (!page || page === '0') return redirect('/transactions/1');
   const txs = await getLastTxs({ page: Number(page) });
   return <TxList transactions={txs} page={page} />;
 }
