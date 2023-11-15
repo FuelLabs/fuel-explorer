d85c0719f6c1574746225f3c2ef4f2a8edcbb897
Matt Auer <mattauer@umich.edu>
2023-11-15 10:26:46 -0600

    feat: add search action

--- packages/app/src/systems/Core/components/SearchInput/SearchInput.tsx
+++ packages/app/src/systems/Core/components/SearchInput/SearchInput.tsx
@@ -1,8 +1,9 @@
 'use client';
 import type { BaseProps, InputFieldProps, InputProps } from '@fuels/ui';
-import { Focus, Icon, IconButton, Tooltip, Input } from '@fuels/ui';
+import { Focus, Icon, IconButton, Tooltip, Input, Text } from '@fuels/ui';
 import { IconCheck, IconSearch, IconX } from '@tabler/icons-react';
 import { useRef, useState } from 'react';
+import { search } from '~/systems/Home/actions/search';
 
 import { cx } from '../../utils/cx';
 
@@ -11,17 +12,19 @@ type SearchInputProps = BaseProps<InputProps & InputFieldProps> & {
   onClear?: () => void;
 };
 
-export function SearchInput({
+export async function SearchInput({
   value: initialValue = '',
   className,
   onSubmit,
   onClear,
   autoFocus,
-  placeholder = 'Search transactions...',
+  placeholder = '0x00000000000000000000000000000000000000000000000000000000000000',
   ...props
 }: SearchInputProps) {
   const [value, setValue] = useState<string>(initialValue as string);
   const inputRef = useRef<HTMLInputElement>(null);
+  const searchResult = await search({ query: value });
+  console.log(`searchResult`, searchResult);
 
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     setValue(event.target.value);
@@ -38,39 +41,44 @@ export function SearchInput({
   }
 
   return (
-    <Focus.ArrowNavigator autoFocus={autoFocus}>
-      <Input className={cx(className)} radius="large" size="3">
-        <Input.Slot className="mx-1">
-          <Icon icon={IconSearch} size={16} />
-        </Input.Slot>
-        <Input.Field
-          {...props}
-          ref={inputRef}
-          placeholder={placeholder}
-          value={value}
-          onChange={handleChange}
-        />
-        {Boolean(value.length) && (
+    <>
+      <Focus.ArrowNavigator autoFocus={autoFocus}>
+        <Input className={cx(className)} radius="large" size="3">
           <Input.Slot className="mx-1">
-            <IconButton
-              aria-label="Clear"
-              icon={IconX}
-              iconColor="text-icon"
-              variant="link"
-              onClick={handleClear}
-            />
-            <Tooltip content="Submit">
-              <IconButton
-                aria-label="Submit"
-                icon={IconCheck}
-                iconColor="text-brand"
-                variant="link"
-                onClick={handleSubmit}
-              />
-            </Tooltip>
+            <Icon icon={IconSearch} size={16} />
           </Input.Slot>
-        )}
-      </Input>
-    </Focus.ArrowNavigator>
+          <Input.Field
+            {...props}
+            ref={inputRef}
+            placeholder={placeholder}
+            value={value}
+            onChange={handleChange}
+          />
+          {Boolean(value.length) && (
+            <Input.Slot className="mx-1">
+              <IconButton
+                aria-label="Clear"
+                icon={IconX}
+                iconColor="text-icon"
+                variant="link"
+                onClick={handleClear}
+              />
+              <Tooltip content="Submit">
+                <IconButton
+                  aria-label="Submit"
+                  icon={IconCheck}
+                  iconColor="text-brand"
+                  variant="link"
+                  onClick={handleSubmit}
+                />
+              </Tooltip>
+            </Input.Slot>
+          )}
+        </Input>
+      </Focus.ArrowNavigator>
+      <Text size="2">
+        Search by address, contract id, transaction id, or block id
+      </Text>
+    </>
   );
 }
