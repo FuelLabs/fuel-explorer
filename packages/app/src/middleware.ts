61c040cd668a5457c69e598b3f02f7c88c92e2e0
Pedro Nauck <pedronauck@gmail.com>
2023-11-15 22:23:36 -0300

    fix: remove redirects

--- packages/app/src/middleware.ts
+++ packages/app/src/middleware.ts
@@ -7,27 +7,9 @@ export function middleware(request: NextRequest) {
   if (pathname === '/') {
     return NextResponse.redirect(new URL('/transactions?page=1', request.url));
   }
-  if (pathname.startsWith('/account')) {
-    const id = pathname.split('/')?.[2] ?? null;
-    if (pathname === `/account/${id}`) {
-      return NextResponse.redirect(
-        new URL(`/account/${id}/assets`, request.url),
-      );
-    }
-    return NextResponse.next();
-  }
-  if (pathname.startsWith('/contract')) {
-    const id = pathname.split('/')?.[2] ?? null;
-    if (pathname === `/contract/${id}`) {
-      return NextResponse.redirect(
-        new URL(`/contract/${id}/assets`, request.url),
-      );
-    }
-    return NextResponse.next();
-  }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/', '/account/:path*', '/contract/:path*'],
+  matcher: ['/'],
 };
