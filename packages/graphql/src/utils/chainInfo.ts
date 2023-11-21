6afdf57a0237dab2c840d65d2628fc17d7e253b5
Luiz Gomes - LuizAsFight.eth <8636507+LuizAsFight@users.noreply.github.com>
2023-11-20 22:12:43 -0300

    feat: add chainInfo to graphql context (#124)
    Closes FE-59

---
+++ packages/graphql/src/utils/chainInfo.ts
@@ -0,0 +1,28 @@
+import { gql } from 'graphql-request';
+
+import { getClient } from './client';
+
+/** @todo: Get types from the query directly instead of creating custom types */
+export type ChainInfoResult = {
+  consensusParameters: {
+    gasPriceFactor: string;
+    gasPerByte: string;
+  };
+};
+export async function getChainInfo(url: string) {
+  const gqlQuery = gql`
+    query chainInfo {
+      chain {
+        consensusParameters {
+          gasPriceFactor
+          gasPerByte
+        }
+      }
+    }
+  `;
+
+  const client = getClient(url);
+  const data = await client.request<{ chain: ChainInfoResult }>(gqlQuery);
+
+  return data.chain;
+}
