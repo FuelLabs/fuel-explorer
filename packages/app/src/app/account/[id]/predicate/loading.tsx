eba665c5b5d39a5c1814c7f59bf369b2ac3b26a1
LuizAsFight <felipebolsonigomes@gmail.com>
2023-11-23 18:23:53 -0300

    fix

---
+++ packages/app/src/app/account/[id]/predicate/loading.tsx
@@ -0,0 +1,13 @@
+import { AccountPredicateLoader } from '~/systems/Account/components/AccountPredicate/AccountPredicateLoader';
+import { AccountTabs } from '~/systems/Account/components/AccountTabs/AccountTabs';
+import { AccountTitle } from '~/systems/Account/components/AccountTitle/AccountTitle';
+
+export default function Loading() {
+  return (
+    <>
+      <AccountTitle isLoading id={''} />
+      <AccountTabs isLoading />
+      <AccountPredicateLoader />
+    </>
+  );
+}
---
+++ packages/app/src/app/account/[id]/transactions/loading.tsx
@@ -0,0 +1,13 @@
+import { AccountTabs } from '~/systems/Account/components/AccountTabs/AccountTabs';
+import { AccountTitle } from '~/systems/Account/components/AccountTitle/AccountTitle';
+import { TxListLoader } from '~/systems/Transaction/component/TxList/TxListLoader';
+
+export default function Loading() {
+  return (
+    <>
+      <AccountTitle isLoading id={''} />
+      <AccountTabs isLoading />
+      <TxListLoader />
+    </>
+  );
+}
