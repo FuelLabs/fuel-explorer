88f430a9ec9381e62a2d0ca76a2276d70ed53a3d
Matt Auer <mattauer@umich.edu>
2023-11-28 12:21:49 -0600

    refactor: remove unused props

--- packages/app/src/systems/Core/components/SearchForm/SearchForm.tsx
+++ packages/app/src/systems/Core/components/SearchForm/SearchForm.tsx
@@ -1,13 +1,10 @@
 import type { SearchResult } from '@fuel-explorer/graphql';
-import type { BaseProps, InputProps } from '@fuels/ui';
 import { useFormState } from 'react-dom';
 import { search } from '~/systems/Core/actions/search';
 
 import { SearchInput } from '../SearchInput/SearchInput';
 
-type SearchFormProps = BaseProps<InputProps>;
-
-export function SearchForm({ className }: SearchFormProps) {
+export function SearchForm({ className }: { className: string }) {
   const [results, action] = useFormState(
     (_: SearchResult | null, formData: FormData) => {
       return search({ query: formData.get('query')?.toString() || '' });
