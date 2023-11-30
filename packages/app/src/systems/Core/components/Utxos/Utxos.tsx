702e935b2ef03f4af25e11c0e1bba2870de16f3d
Matt Auer <mattauer@umich.edu>
2023-11-30 13:40:58 -0600

    refactor: use virtual list

--- packages/app/src/systems/Core/components/Utxos/Utxos.tsx
+++ packages/app/src/systems/Core/components/Utxos/Utxos.tsx
@@ -6,14 +6,12 @@ import {
   Collapsible,
   useBreakpoints,
   Box,
-  ScrollArea,
 } from '@fuels/ui';
 import type { BoxProps } from '@fuels/ui';
 import { IconCoins } from '@tabler/icons-react';
 import { bn } from 'fuels';
 import NextLink from 'next/link';
-import type { SyntheticEvent } from 'react';
-import { useState } from 'react';
+import { FixedSizeList as List } from 'react-window';
 import { tv } from 'tailwind-variants';
 import { useAsset } from '~/systems/Asset/hooks/useAsset';
 import { useFuelAsset } from '~/systems/Asset/hooks/useFuelAsset';
@@ -26,9 +24,10 @@ type UtxoItemProps = {
   item: UtxoItem;
   assetId?: string;
   style?: React.CSSProperties;
+  index: number;
 };
 
-function UtxoItem({ item, style, assetId }: UtxoItemProps) {
+function UtxoItem({ item, style, assetId, index }: UtxoItemProps) {
   const { isMobile } = useBreakpoints();
   const asset = useAsset(assetId);
   const fuelAsset = useFuelAsset(asset);
@@ -36,10 +35,13 @@ function UtxoItem({ item, style, assetId }: UtxoItemProps) {
   if (!item.utxoId) return null;
   if (!asset) return null;
 
-  const classes = styles();
   const trim = isMobile ? 8 : 16;
+  const { item: itemStyle } = styles({
+    color: index % 2 !== 0 ? 'odd' : undefined,
+  });
+
   return (
-    <Box style={style} className={classes.item()}>
+    <Box style={style} className={itemStyle()}>
       <Address
         prefix="ID:"
         value={item.utxoId}
@@ -69,32 +71,32 @@ type UtxosProps = BoxProps & {
   items?: UtxoItem[] | null;
 };
 
-function CommonList({ items, assetId }: UtxosProps) {
-  const [lastItemIdx, setLastItemIdx] = useState(9);
-
-  function handleScroll(e: SyntheticEvent) {
-    const target = e.target as HTMLElement;
-    const bottom =
-      target.scrollHeight - target.scrollTop === target.clientHeight;
-    if (bottom) {
-      setLastItemIdx(lastItemIdx + 10);
-    }
-  }
-
-  console.log(`items?.length`, items?.length);
-
+function VirtualList({ items, assetId }: UtxosProps) {
+  const { isMobile } = useBreakpoints();
+  const itemSize = isMobile ? 85 : 35;
+  const len = items?.length ?? 0;
   return (
-    <ScrollArea
-      scrollbars="vertical"
-      onScroll={handleScroll}
-      className={(items?.length ?? 0) >= 10 ? 'h-80' : ''}
+    <List
+      height={(items?.length ?? 0) >= 10 ? 350 : itemSize * len}
+      itemCount={items?.length ?? 0}
+      width="100%"
+      itemSize={isMobile ? 85 : 35}
     >
-      {items
-        ?.slice(0, lastItemIdx)
-        .map((item) => (
-          <UtxoItem key={item.utxoId} item={item} assetId={assetId} />
-        )) ?? null}
-    </ScrollArea>
+      {({ index: idx, style }) => {
+        const item = items?.[idx];
+        return (
+          item && (
+            <UtxoItem
+              key={item.utxoId}
+              style={style}
+              item={item}
+              assetId={assetId}
+              index={idx}
+            />
+          )
+        );
+      }}
+    </List>
   );
 }
 
@@ -105,7 +107,7 @@ export function Utxos({ items, assetId, ...props }: UtxosProps) {
         UTXOs ({items?.length ?? 0})
       </Collapsible.Title>
       <Collapsible.Body className="p-0">
-        <CommonList items={items} assetId={assetId} />
+        <VirtualList items={items} assetId={assetId} />
       </Collapsible.Body>
     </Collapsible.Content>
   );
@@ -114,10 +116,17 @@ export function Utxos({ items, assetId, ...props }: UtxosProps) {
 const styles = tv({
   slots: {
     item: [
-      'flex flex-col odd:bg-gray-4 p-2 px-4 gap-2',
+      'flex flex-col p-2 px-4 gap-2',
       'tablet:flex-row',
       'last:rounded-b-sm',
       'fuel-[Address]:text-[0.8rem] fuel-[Address]:leading-none',
     ],
+  },
+  variants: {
+    color: {
+      odd: {
+        item: 'bg-gray-4',
+      },
+    },
   },
 });
