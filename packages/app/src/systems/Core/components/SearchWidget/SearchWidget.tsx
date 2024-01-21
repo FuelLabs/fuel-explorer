b4dbbdac42cda16bf4cda3be08767a6dec2cd160
Matt <54373384+matt-user@users.noreply.github.com>
2024-01-21 00:00:52 -0500

    feat: nav search (#123)
    closes FE-14
    
    ---------
    
    Co-authored-by: Pedro Nauck <pedronauck@gmail.com>

---
+++ packages/app/src/systems/Core/components/SearchWidget/SearchWidget.tsx
@@ -0,0 +1,136 @@
+import { HStack, IconButton, Tooltip } from '@fuels/ui';
+import { IconSearch } from '@tabler/icons-react';
+import { AnimatePresence, motion } from 'framer-motion';
+import type { MutableRefObject } from 'react';
+import { useEffect, useRef, createContext } from 'react';
+import { tv } from 'tailwind-variants';
+
+import { SearchForm } from '../SearchForm/SearchForm';
+
+export const SearchContext = createContext<{
+  dropdownRef: null | MutableRefObject<HTMLDivElement | null>;
+  onClear: (value: string) => void;
+}>({ dropdownRef: null, onClear: () => {} });
+
+type SearchWidgetProps = {
+  setIsExitComplete: (value: boolean) => void;
+  isExitComplete: boolean;
+  isSearchOpen: boolean;
+  setIsSearchOpen: (value: boolean) => void;
+};
+
+export const SearchWidget = ({
+  setIsExitComplete,
+  isExitComplete,
+  isSearchOpen,
+  setIsSearchOpen,
+}: SearchWidgetProps) => {
+  const classes = styles();
+  const widgetRef = useRef<HTMLDivElement>(null);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  function onClear(value: string) {
+    if (!value.length) {
+      setIsSearchOpen(false);
+    }
+  }
+
+  function isClickInBounds(
+    clickX: number,
+    clickY: number,
+    left: number,
+    right: number,
+    top: number,
+    bottom: number,
+  ) {
+    return (
+      clickX >= left && clickX <= right && clickY >= top && clickY <= bottom
+    );
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const widgetLeft = widgetRef.current?.offsetLeft ?? 0;
+      const widgetRight = widgetLeft + (widgetRef.current?.offsetWidth ?? 0);
+      const widgetTop = widgetRef.current?.offsetTop ?? 0;
+      const widgetBottom = widgetTop + (widgetRef.current?.offsetHeight ?? 0);
+      if (
+        !isClickInBounds(
+          event.x,
+          event.y,
+          widgetLeft,
+          widgetRight,
+          widgetTop,
+          widgetBottom,
+        ) &&
+        !dropdownRef?.current?.contains(event.target as Node)
+      ) {
+        setIsSearchOpen(false);
+      }
+    };
+    document.addEventListener('click', handleClickOutside, true);
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isSearchOpen) {
+      setIsExitComplete(false);
+    }
+  }, [isSearchOpen]);
+
+  return (
+    <SearchContext.Provider value={{ dropdownRef, onClear }}>
+      <HStack
+        ref={widgetRef}
+        className="items-center gap-0 laptop:gap-4 justify-center"
+      >
+        <AnimatePresence
+          onExitComplete={() => {
+            setIsExitComplete(true);
+          }}
+        >
+          {isSearchOpen && (
+            <>
+              <motion.div
+                initial="closed"
+                animate="open"
+                exit="closed"
+                transition={{ duration: 0.2 }}
+                variants={{
+                  open: { scaleX: '100%' },
+                  closed: { scaleX: '0%' },
+                }}
+              >
+                <SearchForm
+                  className={classes.input()}
+                  autoFocus={true}
+                  alwaysDisplayActionButtons={true}
+                />
+              </motion.div>
+            </>
+          )}
+        </AnimatePresence>
+        {isExitComplete && (
+          <Tooltip content="Search by address, contract id, transaction id, or block id">
+            <IconButton
+              icon={IconSearch}
+              variant="link"
+              className="mr-1 text-color laptop:mr-0"
+              onClick={() => {
+                setIsSearchOpen(!isSearchOpen);
+              }}
+            />
+          </Tooltip>
+        )}
+      </HStack>
+    </SearchContext.Provider>
+  );
+};
+
+const styles = tv({
+  slots: {
+    input: 'w-full tablet:w-[400px]',
+  },
+});
--- packages/app/src/systems/Core/components/TopNav/TopNav.tsx
+++ packages/app/src/systems/Core/components/TopNav/TopNav.tsx
@@ -1,11 +1,29 @@
 'use client';
 
-import { Nav } from '@fuels/ui';
+import { Nav, useBreakpoints } from '@fuels/ui';
 import NextLink from 'next/link';
+import { useEffect, useState } from 'react';
 
 import { setTheme } from '../../actions/setTheme';
+import { SearchWidget } from '../SearchWidget/SearchWidget';
 
 export function TopNav() {
+  // We need two of each variable bc both the mobile and desktop
+  // nav elements are in the DOM and respond to click events.
+  const [isDesktopExitComplete, setIsDesktopExitComplete] = useState(true);
+  const [isMobileExitComplete, setIsMobileExitComplete] = useState(true);
+  const [isDesktopSearchOpen, setIsDesktopSearchOpen] = useState(false);
+  const [isMobileSearchOpen, setIsMobileSearchOpen] = useState(false);
+  const { isLaptop } = useBreakpoints();
+
+  useEffect(() => {
+    if (isLaptop && isMobileSearchOpen) {
+      setIsDesktopSearchOpen(true);
+    } else if (!isLaptop && isDesktopSearchOpen) {
+      setIsMobileSearchOpen(true);
+    }
+  }, [isLaptop]);
+
   const logo = (
     <NextLink href="/" className="flex items-center flex-1 laptop:flex-initial">
       <Nav.Logo />
@@ -31,7 +49,12 @@ export function TopNav() {
       >
         Bridge
       </Nav.MenuItem>
-      <Nav.MenuItem isActive as={NextLink} href="/">
+      <Nav.MenuItem
+        isActive
+        as={NextLink}
+        href="/"
+        className="flex items-center"
+      >
         Explorer
       </Nav.MenuItem>
       <Nav.MenuItem
@@ -50,22 +73,44 @@ export function TopNav() {
 
   return (
     <Nav>
-      <Nav.Desktop className="px-10">
+      <Nav.Desktop
+        className={`px-10 ${!isDesktopExitComplete ? 'justify-between' : ''}`}
+      >
         {logo}
-        <Nav.Menu>{externalLinks}</Nav.Menu>
-        <Nav.Spacer />
-        <Nav.Menu>{tooling}</Nav.Menu>
-        {themeToggle}
+        {isDesktopExitComplete && (
+          <>
+            <Nav.Menu>{externalLinks}</Nav.Menu>
+            <Nav.Spacer />
+          </>
+        )}
+        <Nav.Menu>
+          <SearchWidget
+            setIsExitComplete={setIsDesktopExitComplete}
+            isExitComplete={isDesktopExitComplete}
+            setIsSearchOpen={setIsDesktopSearchOpen}
+            isSearchOpen={isDesktopSearchOpen}
+          />
+          {isDesktopExitComplete && tooling}
+        </Nav.Menu>
+        {!isDesktopExitComplete ? <div></div> : themeToggle}
       </Nav.Desktop>
       <Nav.Mobile>
         <Nav.MobileContent>
           {logo}
-          {themeToggle}
+          <SearchWidget
+            setIsExitComplete={setIsMobileExitComplete}
+            isExitComplete={isMobileExitComplete}
+            setIsSearchOpen={setIsMobileSearchOpen}
+            isSearchOpen={isMobileSearchOpen}
+          />
+          {!isMobileExitComplete ? <div></div> : themeToggle}
         </Nav.MobileContent>
-        <Nav.Menu>
-          {externalLinks}
-          {tooling}
-        </Nav.Menu>
+        {isMobileExitComplete && (
+          <Nav.Menu>
+            {externalLinks}
+            {tooling}
+          </Nav.Menu>
+        )}
       </Nav.Mobile>
     </Nav>
   );
