0b96700d4b3339bc9b0e7754d996618c8f924205
Pedro Nauck <pedronauck@gmail.com>
2024-02-05 18:09:32 -0300

    chore: lintstaged

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,6 +1,3 @@
 export default {
-  "**/*.(js|jsx|ts|tsx|md|mdx|html|css)": [
-    "biome format --write",
-    "biome lint --apply-unsafe",
-  ],
+  "**/*.(js|jsx|ts|tsx|md|mdx|html|css)": ["biome check --apply-unsafe"],
 };
