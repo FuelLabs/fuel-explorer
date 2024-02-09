85fafbec9e98c2e7d68064ce5eaef16b45db7306
luizstacio <luizstacio@gmail.com>
2024-02-09 14:54:38 -0300

    chore: revert changes on main

--- packages/app-portal/src/main.tsx
+++ packages/app-portal/src/main.tsx
@@ -5,11 +5,8 @@ import './polyfills';
 
 import { App } from './App';
 
-// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// );
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
