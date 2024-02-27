fa55913eead793960a59e6f710cd5f497d6a70db
Hélcio Franco <helciofranco@me.com>
2024-02-27 12:26:03 -0400

    fix: explorer bg color on dark theme (#201)

--- packages/app-portal/src/systems/Ecosystem/components/EcosystemTags/EcosystemTags.tsx
+++ packages/app-portal/src/systems/Ecosystem/components/EcosystemTags/EcosystemTags.tsx
@@ -25,7 +25,7 @@ export const EcosystemTags = ({
         size="2"
         variant="outline"
         color={!activeTag ? 'green' : 'gray'}
-        className="bg-white"
+        className="bg-panel-solid"
         onClick={onClickAllCategories}
       >
         All categories
@@ -35,7 +35,7 @@ export const EcosystemTags = ({
         <Button
           key={tag}
           color={activeTag === tag ? 'green' : 'gray'}
-          className="bg-white"
+          className="bg-panel-solid"
           size="2"
           variant="outline"
           onClick={() => onClickTag?.(tag)}
