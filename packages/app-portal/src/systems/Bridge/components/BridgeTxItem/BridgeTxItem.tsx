132bfa5ce1c8a6f4a9b7dc8da6ba03f66247e4d2
Hélcio Franco <helciofranco@me.com>
2024-03-04 17:46:43 -0400

    fix: show more width and card height

--- packages/app-portal/src/systems/Bridge/components/BridgeTxItem/BridgeTxItem.tsx
+++ packages/app-portal/src/systems/Bridge/components/BridgeTxItem/BridgeTxItem.tsx
@@ -76,7 +76,7 @@ export const BridgeTxItem = ({
 const styles = tv({
   slots: {
     networks: 'gap-1',
-    cardItem: 'flex flex-row min-h-[24px] gap-6 items-center',
+    cardItem: 'flex flex-row min-h-[58px] gap-6 items-center',
     statusTime: [
       'flex-1 justify-between',
       'mobile:max-tablet:flex-col-reverse mobile:max-tablet:flex-wrap',
