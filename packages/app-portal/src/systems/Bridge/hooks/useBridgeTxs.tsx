7d434ad5388675d653f99872df3892f8ce1614e4
Hélcio Franco <helciofranco@me.com>
2024-03-05 18:51:25 -0400

    fix: wait fuel ready state before fire fuels requests

--- packages/app-portal/src/systems/Bridge/hooks/useBridgeTxs.tsx
+++ packages/app-portal/src/systems/Bridge/hooks/useBridgeTxs.tsx
@@ -24,15 +24,17 @@ export const useBridgeTxs = () => {
   const { data: bridgeTxs, isLoading: isLoadingState } = useQuery({
     queryKey: ['bridgeTxs'],
     queryFn: async () => {
-      const data = await BridgeService.fetchTxs({
+      return await BridgeService.fetchTxs({
         fuelAddress,
         fuelProvider,
         ethPublicClient,
       });
-
-      return data;
     },
-    enabled: !!fuelAddress && !!fuelProvider && !!ethPublicClient,
+    enabled:
+      !!fuelAddress &&
+      !!fuelProvider &&
+      !!ethPublicClient &&
+      !isLoadingConnection,
     // @TODO: Move it to global the setting, basically it keeps everything on cache but always getting once fresh data
     cacheTime: 1000 * 60, // 1 minute
     staleTime: Infinity,
