29c056f3cbf9976e2bf3f77af84890ef7b7155ff
Hélcio Franco <helciofranco@me.com>
2024-03-22 12:22:11 -0400

    chore: revert invalidation

--- packages/app-portal/src/systems/Chains/eth/hooks/useEthBalance.tsx
+++ packages/app-portal/src/systems/Chains/eth/hooks/useEthBalance.tsx
@@ -1,21 +1,11 @@
-import { useAccount, useBalance, useBlockNumber } from 'wagmi';
-
-import { useQueryClient } from '@tanstack/react-query';
-import { useEffect } from 'react';
+import { useAccount, useBalance } from 'wagmi';
 
 export function useEthBalance(token?: `0x${string}`) {
-  const queryClient = useQueryClient();
-
   const { address } = useAccount();
-  const { data: blockNumber } = useBlockNumber({ watch: true });
-  const { data: ethBalance, queryKey: ethBalanceQueryKey } = useBalance({
+  const { data: ethBalance } = useBalance({
     address,
     token,
   });
-
-  useEffect(() => {
-    queryClient.invalidateQueries({ queryKey: ethBalanceQueryKey });
-  }, [queryClient, blockNumber, ethBalanceQueryKey]);
 
   return {
     ethBalance,
