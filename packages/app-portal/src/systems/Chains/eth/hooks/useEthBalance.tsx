9e8cf55905e5188612da23e57fb92f27490134cf
Hélcio Franco <helciofranco@me.com>
2024-03-22 14:39:12 -0400

    Revert "chore: revert invalidation"
    This reverts commit 29c056f3cbf9976e2bf3f77af84890ef7b7155ff.

--- packages/app-portal/src/systems/Chains/eth/hooks/useEthBalance.tsx
+++ packages/app-portal/src/systems/Chains/eth/hooks/useEthBalance.tsx
@@ -1,11 +1,21 @@
-import { useAccount, useBalance } from 'wagmi';
+import { useAccount, useBalance, useBlockNumber } from 'wagmi';
+
+import { useQueryClient } from '@tanstack/react-query';
+import { useEffect } from 'react';
 
 export function useEthBalance(token?: `0x${string}`) {
+  const queryClient = useQueryClient();
+
   const { address } = useAccount();
-  const { data: ethBalance } = useBalance({
+  const { data: blockNumber } = useBlockNumber({ watch: true });
+  const { data: ethBalance, queryKey: ethBalanceQueryKey } = useBalance({
     address,
     token,
   });
+
+  useEffect(() => {
+    queryClient.invalidateQueries({ queryKey: ethBalanceQueryKey });
+  }, [queryClient, blockNumber, ethBalanceQueryKey]);
 
   return {
     ethBalance,
