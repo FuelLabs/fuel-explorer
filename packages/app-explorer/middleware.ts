12629b888fb2dcf40f45ef0ae20bc82487efa38c
LuizAsFight <felipebolsonigomes@gmail.com>
2024-03-28 04:22:10 -0300

    chore: test middleware

--- packages/app-explorer/middleware.ts
+++ packages/app-explorer/middleware.ts
@@ -1,3 +1,11 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(new URL('/home', request.url));
+}
+
 export const config = {
   matcher: [
     /*
