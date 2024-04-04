d424064f4ae7953650557c36ec9ee852034e2543
Hélcio Franco <helciofranco@me.com>
2024-04-04 17:17:02 -0400

    feat: add resolver

--- packages/graphql-new/src/domain/BridgeContractLog/BridgeContractLogRepository.ts
+++ packages/graphql-new/src/domain/BridgeContractLog/BridgeContractLogRepository.ts
@@ -1,6 +1,7 @@
-import { desc, eq, sql } from 'drizzle-orm';
+import { SQL, and, desc, eq, sql } from 'drizzle-orm';
 
-import { Paginator, PaginatorParams } from '~/core/Paginator';
+import { Paginator } from '~/core/Paginator';
+import { GQLQueryBridgeContractLogsArgs } from '~/graphql/generated/sdk';
 import { db } from '~/infra/database/Db';
 import {
   BridgeBlocksTable,
@@ -11,10 +12,31 @@ import { BridgeContractLogEntity } from './BridgeContractLogEntity';
 import { Log } from './vo/BridgeContractLogData';
 
 export class BridgeContractLogRepository {
-  async findMany(params: PaginatorParams): Promise<BridgeContractLogEntity[]> {
+  async findMany(
+    params: GQLQueryBridgeContractLogsArgs,
+  ): Promise<BridgeContractLogEntity[]> {
+    // Filtering by args
+    const where: SQL[] = [];
+    if (params?.args) {
+      const args = params.args;
+      const sqls = {
+        recipient: sql`${BridgeContractLogsTable.args} ->> 'recipient' = ${args.recipient}`,
+        messageId: sql`${BridgeContractLogsTable.args} ->> 'messageId' = ${args.messageId}`,
+      };
+      for (const key of Object.keys(params.args)) {
+        where.push(sqls[key]);
+      }
+    }
+
+    // Regular filtering
+    if (params?.name) {
+      where.push(eq(BridgeContractLogsTable.name, params.name));
+    }
+
+    // Default pagination and sorting
     const paginator = new Paginator(BridgeContractLogsTable, params);
     const config = await paginator.getQueryPaginationConfig();
-    const query = paginator.getPaginatedResult(config);
+    const query = paginator.getPaginatedResult(config, and(...where));
     const results = await query.innerJoin(
       BridgeBlocksTable,
       eq(BridgeBlocksTable._id, BridgeContractLogsTable.blockNumber),
