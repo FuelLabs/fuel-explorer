6cbc22cfd14698aee20757b16d33a4845595b7b2
Luiz Gomes - LuizAsFight.eth <8636507+LuizAsFight@users.noreply.github.com>
2024-04-05 12:04:56 -0300

    fix: remove block watcher to refresh balance (#294)

--- packages/e2e-tests/playwright.config.ts
+++ packages/e2e-tests/playwright.config.ts
@@ -9,7 +9,7 @@ const config: PlaywrightTestConfig = defineConfig({
   workers: 1,
   testMatch: join(__dirname, './bridge/**/*.test.ts'),
   testDir: join(__dirname, './bridge/'),
-  timeout: 60_000 * 10,
+  timeout: 60_000 * 7,
   expect: {
     timeout: 5000,
   },
