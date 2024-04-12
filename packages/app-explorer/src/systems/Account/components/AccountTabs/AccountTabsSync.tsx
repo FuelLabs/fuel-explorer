6960ac123bf712dec1b98ff0960fa2645dbb1241
Hélcio Franco <helciofranco@me.com>
2024-04-12 20:50:07 -0300

    fix: disable predicates tab when necessary

--- packages/app-explorer/src/systems/Account/components/AccountTabs/AccountTabsSync.tsx
+++ packages/app-explorer/src/systems/Account/components/AccountTabs/AccountTabsSync.tsx
@@ -4,5 +4,10 @@ import { AccountTabs } from './AccountTabs';
 
 export async function AccountsTabsSync({ id }: { id: string }) {
   const predicate = await getPredicate({ owner: id });
-  return <AccountTabs address={id} isPredicate={!!predicate?.bytecode} />;
+  return (
+    <AccountTabs
+      address={id}
+      isPredicate={!!predicate?.bytecode && predicate.bytecode !== '0x'}
+    />
+  );
 }
