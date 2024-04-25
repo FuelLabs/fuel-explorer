714a4832ccc3f9853a1e640377cc56d2e55070f1
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-04-25 17:10:02 -0300

    fix: any usage on find asset by id

--- packages/app-explorer/src/systems/Core/utils/asset.ts
+++ packages/app-explorer/src/systems/Core/utils/asset.ts
@@ -1,13 +1,14 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { assets, resolveIconPath } from '@fuels/assets';
+import { type Fuel, assets, resolveIconPath } from '@fuels/assets';
 
 export const ASSET_LIST = resolveIconPath('/assets', assets);
 
 export function findAssetById(assetId?: string) {
   return (
     ASSET_LIST.find((asset) => {
-      const network = asset.networks?.find((val) => val.type === 'fuel');
-      const id = network ? (network as any).assetId : null;
+      const network = asset.networks?.find(
+        (val) => val.type === 'fuel',
+      ) as Fuel;
+      const id = network ? network?.assetId : null;
       return id === assetId;
     }) ?? null
   );
