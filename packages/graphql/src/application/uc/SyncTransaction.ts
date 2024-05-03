b455736c34a62ce7ace24519bfaf525e5c6ce1c5
Pedro Nauck <pedronauck@gmail.com>
2024-05-03 14:34:26 -0300

    fix: passing input for worker

--- packages/graphql/src/application/uc/SyncTransaction.ts
+++ packages/graphql/src/application/uc/SyncTransaction.ts
@@ -1,26 +1,19 @@
-import { Address } from '~/core/Address';
-import type { GQLBlock } from '~/graphql/generated/sdk';
 import type { QueueData, QueueInputs, QueueNames } from '~/infra/queue/Queue';
 import { pool } from '~/infra/worker/WorkerPool';
 
 type Input = QueueInputs[QueueNames.SYNC_TRANSACTION];
 
 export class SyncTransaction {
-  constructor(
-    readonly txAddr: Address,
-    readonly block: GQLBlock,
-  ) {}
-
-  async execute(input: Input) {
+  async execute(input: QueueData<Input>) {
     await pool.run(input);
   }
 }
 
-export const syncTransaction = async ({ data }: QueueData<Input>) => {
+export const syncTransaction = async (input: QueueData<Input>) => {
+  const { data } = input;
   try {
-    const txAddr = new Address(data.txHash);
-    const syncTransactions = new SyncTransaction(txAddr, data.block);
-    return syncTransactions.execute(data);
+    const syncTransactions = new SyncTransaction();
+    return syncTransactions.execute(input);
   } catch (error) {
     console.error(error);
     throw new Error(`Sync transactions ${data.txHash}`, {
