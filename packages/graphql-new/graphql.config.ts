49f9a07c3a7a27c0bb4bdb741110eae792bd2e8b
Pedro Nauck <pedronauck@gmail.com>
2024-05-22 11:14:03 +0200

    chore: remove graphql-new package

--- packages/graphql-new/graphql.config.ts
+++
@@ -1,8 +0,0 @@
-import type { IGraphQLConfig } from 'graphql-config';
-
-const config: IGraphQLConfig = {
-  schema: ['./src/graphql/schemas/**.graphql'],
-  documents: ['./src/domain/**/*.graphql'],
-};
-
-export default config;
