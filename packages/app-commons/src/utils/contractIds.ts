f7f8332e76ffb1dbe06a5d749751ce46579e342f
Pedro Nauck <pedronauck@gmail.com>
2024-05-22 12:52:36 +0200

    fix: next build warning

--- packages/app-commons/src/utils/contractIds.ts
+++ packages/app-commons/src/utils/contractIds.ts
@@ -38,25 +38,17 @@ export async function getBridgeTokenContracts() {
 
   if (ETH_CHAIN_NAME === 'sepolia') {
     const ETH_ERC20 = '0xC6387efAD0F184a90B34f397C3d6Fd63135ef790';
-    if (FUEL_CHAIN_NAME === 'fuelBeta5Dev') {
-      bridgeTokenContract = {
-        ETH_ERC20,
-        FUEL_TokenContract:
-          '0x7d3e3721d96108b71e187aa17c3330ac400637a2649490f4b3d964cbf1b8943e',
-      };
+    const contractId =
+      FUEL_CHAIN_NAME !== 'fuelTestnet'
+        ? '0x7d3e3721d96108b71e187aa17c3330ac400637a2649490f4b3d964cbf1b8943e'
+        : '0x84233a3696f4ca759e7f07348f33efa98e1dc1fe65bc1cc5ea693a1368b0f9e9';
 
-      return bridgeTokenContract;
-    }
+    bridgeTokenContract = {
+      ETH_ERC20,
+      FUEL_TokenContract: contractId,
+    };
 
-    if (FUEL_CHAIN_NAME === 'fuelBeta5') {
-      bridgeTokenContract = {
-        ETH_ERC20,
-        FUEL_TokenContract:
-          '0x84233a3696f4ca759e7f07348f33efa98e1dc1fe65bc1cc5ea693a1368b0f9e9',
-      };
-
-      return bridgeTokenContract;
-    }
+    return bridgeTokenContract;
   }
 }
 
@@ -76,27 +68,22 @@ export async function getBridgeSolidityContracts() {
   }
 
   if (ETH_CHAIN_NAME === 'sepolia') {
-    if (FUEL_CHAIN_NAME === 'fuelBeta5Dev') {
-      bridgeSolidityContracts = {
-        FuelChainState: '0xb65850FB7eA866f8730Ce713657ed965407F6472',
-        FuelMessagePortal: '0xBf340BAC79c301B264E2a5dEa51b7F61eb3e666A',
-        FuelERC20Gateway: '0x749E27d070E2F4a3D6CED522a0D4BDCB37fA95ba',
-        FuelERC721Gateway: '0x4aC11e55652b4e13Fc8dB6F42bB26793605d03B8',
-      };
+    bridgeSolidityContracts =
+      FUEL_CHAIN_NAME !== 'fuelTestnet'
+        ? {
+            FuelChainState: '0xb65850FB7eA866f8730Ce713657ed965407F6472',
+            FuelMessagePortal: '0xBf340BAC79c301B264E2a5dEa51b7F61eb3e666A',
+            FuelERC20Gateway: '0x749E27d070E2F4a3D6CED522a0D4BDCB37fA95ba',
+            FuelERC721Gateway: '0x4aC11e55652b4e13Fc8dB6F42bB26793605d03B8',
+          }
+        : {
+            FuelChainState: '0x395B125343ADebCcB05dd70e117774E3AB08a8a7',
+            FuelMessagePortal: '0x557c5cE22F877d975C2cB13D0a961a182d740fD5',
+            FuelERC20Gateway: '0xE52af7c9A2F6b243CEE9F0C423E06BAb6E5c6E3b',
+            FuelERC721Gateway: '0xc094fC648101920B1C37C733AF022942eF4042D3',
+          };
 
-      return bridgeSolidityContracts;
-    }
-
-    if (FUEL_CHAIN_NAME === 'fuelBeta5') {
-      bridgeSolidityContracts = {
-        FuelChainState: '0x395B125343ADebCcB05dd70e117774E3AB08a8a7',
-        FuelMessagePortal: '0x557c5cE22F877d975C2cB13D0a961a182d740fD5',
-        FuelERC20Gateway: '0xE52af7c9A2F6b243CEE9F0C423E06BAb6E5c6E3b',
-        FuelERC721Gateway: '0xc094fC648101920B1C37C733AF022942eF4042D3',
-      };
-
-      return bridgeSolidityContracts;
-    }
+    return bridgeSolidityContracts;
   }
 
   return bridgeSolidityContracts;
--- packages/app-explorer/src/systems/Asset/components/AssetsLoader.tsx
+++ packages/app-explorer/src/systems/Asset/components/AssetsLoader.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 import { VStack } from '@fuels/ui';
-import { BaseAssetId } from 'fuels';
 import { BalanceItem } from '~/systems/Core/components/BalanceItem/BalanceItem';
 
 const PER_PAGE = 4;
@@ -10,7 +9,7 @@ export function AssetsLoader() {
   return (
     <VStack className="min-h-[45vh]">
       {[...Array(PER_PAGE)].map((_, i) => (
-        <BalanceItem key={i} isLoading item={{ assetId: BaseAssetId } as any} />
+        <BalanceItem key={i} isLoading item={{ assetId: '0x00' } as any} />
       ))}
     </VStack>
   );
