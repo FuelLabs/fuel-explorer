f45725eab055487e53a109c035f3db27d5a0804a
LuizAsFight <felipebolsonigomes@gmail.com>
2024-05-24 18:13:14 +0200

    chore: playwright config

--- packages/e2e-tests/playwright.config.ts
+++ packages/e2e-tests/playwright.config.ts
@@ -2,14 +2,14 @@ import { join } from 'path';
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { defineConfig, devices } from '@playwright/test';
 
-const IS_CI = !!process.env.CI;
+const _IS_CI = !!process.env.CI;
 const PORT = process.env.PORT || 3001;
 
 const config: PlaywrightTestConfig = defineConfig({
   workers: 1,
   testMatch: join(__dirname, './bridge/**/*.test.ts'),
   testDir: join(__dirname, './bridge/'),
-  timeout: 60_000 * 7,
+  timeout: 60_000 * 10,
   expect: {
     timeout: 5000,
   },
@@ -17,7 +17,7 @@ const config: PlaywrightTestConfig = defineConfig({
   // Fail the build on CI if left test.only in the source code
   forbidOnly: !!process.env.CI,
   // Retry tests on CI if they fail
-  retries: IS_CI ? 2 : 0,
+  retries: 0,
   webServer: {
     command: 'pnpm dev',
     port: Number(PORT),
