e2a12002bddca6d3530f62d8c6785f51183ffe8b
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-06-03 19:56:27 -0300

    fix: "as" breaking changes

--- packages/ui/src/components/Collapsible/Collapsible.stories.tsx
+++ packages/ui/src/components/Collapsible/Collapsible.stories.tsx
@@ -47,7 +47,7 @@ export const Variants: Story = {
 
 export const Polymorphic: Story = {
   render: () => (
-    <Collapsible as="article" className="max-w-[450px]">
+    <Collapsible as="div" className="max-w-[450px]">
       <Collapsible.Header>Header</Collapsible.Header>
       <Collapsible.Content>
         <Collapsible.Title>Title</Collapsible.Title>
