7279daa920d5a49533b1fd400eec1d81e6a461b4
Pedro Nauck <pedronauck@gmail.com>
2024-06-13 18:14:03 -0300

    chore: remove old graphql package

--- packages/graphql/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { createSchema } from './schema';
-export type { GraphQLExecutor } from './utils/executor';
-export { createExecutor } from './utils/executor';
-export * as mocks from './generated/mocks';
-export * from './generated/types';
