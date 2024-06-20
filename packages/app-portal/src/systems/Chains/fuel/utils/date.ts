c2482c5711a3ece62b2fad678baca8598bc95f73
Rodrigo Branas <rodrigo.branas@gmail.com>
2024-06-20 12:46:25 -0300

    fix: timing on bridge bugged in first minute (#383)

--- packages/app-portal/src/systems/Chains/fuel/utils/date.ts
+++ packages/app-portal/src/systems/Chains/fuel/utils/date.ts
@@ -1,10 +1,10 @@
-import dayjs from 'dayjs';
+import { createDayjs } from 'app-commons/src/utils/dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
-dayjs.extend(relativeTime, {
-  rounding: (num) => Math.ceil(num),
-});
-
 export const distanceToNow = (fromDate: Date) => {
+  const dayjs = createDayjs();
+  dayjs.extend(relativeTime, {
+    rounding: (num) => Math.ceil(num),
+  });
   return dayjs(fromDate).toNow(true);
 };
