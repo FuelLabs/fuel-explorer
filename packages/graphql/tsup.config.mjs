e92e08896b339445626364301f56cb3c0407ec70
Pedro Nauck <pedronauck@gmail.com>
2024-06-25 21:31:53 -0300

    fix: server build files

--- packages/graphql/tsup.config.mjs
+++ packages/graphql/tsup.config.mjs
@@ -1,19 +1,36 @@
 import { defineConfig } from 'tsup';
+import { join } from 'node:path';
+import fs from 'fs-extra';
 
-const { SERVER_BUILD } = process.env;
-const isServerBuild = SERVER_BUILD === 'true';
-
-export default defineConfig(() => ({
-  outDir: 'dist',
-  splitting: true,
-  format: ['esm', 'cjs'],
-  sourcemap: true,
-  clean: false,
-  dts: !isServerBuild,
-  minify: false,
-  entry: {
-    index: 'src/index.ts',
-    app: 'src/app.ts',
-    syncer: 'src/syncer.ts',
+export default defineConfig(() => [
+  {
+    outDir: 'dist',
+    splitting: false,
+    format: ['cjs'],
+    sourcemap: true,
+    clean: true,
+    dts: false,
+    minify: false,
+    entry: {
+      index: 'src/index.ts',
+      app: 'src/app.ts',
+      syncer: 'src/syncer.ts',
+    },
   },
-}));
+  {
+    outDir: 'dist/resolvers',
+    splitting: false,
+    format: ['cjs'],
+    sourcemap: false,
+    clean: true,
+    dts: false,
+    minify: false,
+    entry: ['src/graphql/resolvers/**.ts'],
+    async onSuccess() {
+      const cwd = process.cwd();
+      const dirPath = join(cwd, 'dist/schemas');
+      const srcPath = join(cwd, 'src/graphql/schemas');
+      await fs.copy(srcPath, dirPath);
+    },
+  },
+]);
