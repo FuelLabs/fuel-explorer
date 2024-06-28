d5b47566bdc8f5212271f4f9f754a3dd2784b628
Pedro Nauck <pedronauck@gmail.com>
2024-06-28 01:05:22 -0300

    fix: script to fix schema for cluster

--- packages/graphql/scripts/fix-schema.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "$0"
-filename="./src/graphql/schemas/fuelcore.graphql"
-echo "Fixing schema file $filename"
-file=$(sed 's/witnessIndex: Int!/witnessIndex: U16!/g' "$filename")
-echo "$file" >"$filename"
