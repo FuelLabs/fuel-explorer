b6725a33abbab775ed38e122d9d12e14e0f98a0d
Pedro Nauck <pedronauck@gmail.com>
2024-07-01 15:31:56 -0300

    fix: syncer default sync missing

--- packages/graphql/src/syncer.ts
+++ packages/graphql/src/syncer.ts
@@ -1,17 +1,19 @@
+import { setTimeout } from 'node:timers/promises';
+import { env } from './config';
 import { logger } from './core/Logger';
-import { mq } from './infra/queue/Queue';
+import { QueueNames, mq } from './infra/queue/Queue';
 
 async function main() {
   await mq.setup();
   logger.info('🚀 Syncer is running');
   logger.info('----');
 
-  // if (env.get('SYNC_MISSING')) {
-  //   logger.info('🕐 Syncing missing blocks in 5 seconds...');
-  //   await setTimeout(5000);
-  //   await mq.send('main', QueueNames.SYNC_MISSING);
-  //   logger.info('📝 Syncer logs will be available in logs/syncer.log file');
-  // }
+  if (env.get('SYNC_MISSING')) {
+    logger.info('🕐 Syncing missing blocks in 5 seconds...');
+    await setTimeout(5000);
+    await mq.send('main', QueueNames.SYNC_MISSING);
+    logger.info('📝 Syncer logs will be available in logs/syncer.log file');
+  }
 }
 
 (async () => {
