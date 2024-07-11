fd53523b5a95be68c8ab72ff75cfac2e83cbc67c
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-07-11 18:53:32 -0300

    feat: replace click on link behavior with useRouter

--- packages/ui/src/components/ToggleGroup/ToggleGroup.tsx
+++ packages/ui/src/components/ToggleGroup/ToggleGroup.tsx
@@ -1,5 +1,6 @@
 import { SegmentedControl as SC } from '@radix-ui/themes';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { createComponent, withNamespace } from '../../utils/component';
 import type { PropsOf } from '../../utils/types';
 import { styles } from './styles';
@@ -26,22 +27,24 @@ export const ToggleGroupItem = createComponent<
   baseElement: SC.Item,
   className: ({ className }) => styles().item({ className }),
   render: (Item, { children, href, ...props }) => {
+    const router = useRouter();
     if (!href) return <Item {...props}>{children}</Item>;
+    const { onClick: _onClick, ...rest } = props;
 
-    function onLinkClick(e: React.MouseEvent<HTMLAnchorElement>) {
-      e.currentTarget.closest('button')?.click();
+    function onClick(e: React.MouseEvent<HTMLButtonElement>) {
+      _onClick?.(e);
+      !!href && router.push(href);
     }
 
     return (
-      <Item {...props}>
+      <Item onClick={onClick} {...rest}>
+        {children}
         <Link
           href={href}
           prefetch
-          className="h-[inherit]"
-          onClick={onLinkClick}
-        >
-          {children}
-        </Link>
+          aria-hidden
+          className="absolute z-[-1] invisible"
+        />
       </Item>
     );
   },
