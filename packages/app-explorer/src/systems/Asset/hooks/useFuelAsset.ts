12240d7b24f4e5188e7513165f40382fcd803527
Rodrigo Branas <rodrigo.branas@gmail.com>
2024-07-12 12:17:08 -0300

    fix: formatting base asset on testnet (2) (#420)

--- packages/app-explorer/src/systems/Asset/hooks/useFuelAsset.ts
+++ packages/app-explorer/src/systems/Asset/hooks/useFuelAsset.ts
@@ -1,10 +1,10 @@
-import { CHAIN_IDS, getAssetFuel } from '@fuels/assets';
-import type { Asset } from '@fuels/assets';
+import { CHAIN_IDS, getAssetFuel } from '@fuel-ts/account';
+import type { Asset } from '@fuel-ts/account';
 import { useMemo } from 'react';
 
 export const useFuelAsset = (asset?: Asset | null) => {
   const fuelAsset = useMemo(
-    () => (asset ? getAssetFuel(asset, CHAIN_IDS.fuel.beta4) : undefined),
+    () => (asset ? getAssetFuel(asset, CHAIN_IDS.fuel.testnet) : undefined),
     [asset?.symbol, asset?.name, asset?.networks.length],
   );
 
