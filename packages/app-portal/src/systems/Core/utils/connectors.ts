77f245d0e39adb5781ffa7281ae62ff4221725ff
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-07-19 18:42:19 -0300

    feat: use default config for connectors

--- packages/app-portal/src/systems/Core/utils/connectors.ts
+++ packages/app-portal/src/systems/Core/utils/connectors.ts
@@ -1,25 +1,17 @@
 import { WALLETCONNECT_ID } from 'app-commons';
 import { type CreateConnectorFn } from 'wagmi';
-import {
-  coinbaseWallet,
-  injected,
-  metaMask,
-  walletConnect,
-} from 'wagmi/connectors';
+import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors';
 import { APP } from '~portal/systems/Chains';
 
-export function generateETHConnectors(
-  useInjected = true,
-): Array<CreateConnectorFn> {
+export function generateETHConnectors(): Array<CreateConnectorFn> {
   const connectors: Array<CreateConnectorFn> = [
-    useInjected
-      ? injected({
-          shimDisconnect: true,
-          target: 'metaMask',
-        })
-      : metaMask(),
+    injected({
+      shimDisconnect: true,
+      target: 'metaMask',
+    }),
     coinbaseWallet({ appName: APP.name, headlessMode: true }),
   ];
+
   if (WALLETCONNECT_ID) {
     connectors.push(
       walletConnect({
