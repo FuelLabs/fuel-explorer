a4e427a64527ac8a1474215a18465622b435feee
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-07-22 13:48:12 -0300

    feat: disconnect both sides of bridge on disconnect Wallet Connect connector

--- packages/app-portal/src/systems/Chains/eth/hooks/useSyncEthWallets.ts
+++ packages/app-portal/src/systems/Chains/eth/hooks/useSyncEthWallets.ts
@@ -1,5 +1,6 @@
 import { useDisconnect as useDisconnectFuel, useFuel } from '@fuels/react';
 import { toast } from '@fuels/ui';
+import { FuelConnectorEventTypes } from 'fuels';
 
 import { useEffect } from 'react';
 import {
@@ -12,10 +13,11 @@ import {
 const WalletConnectName = 'Ethereum Wallets';
 
 /**
- * @description This hooks exists to align the bridge with the same account in the alternate wallet when using Ethereum Wallets on the Fuel's side in bridge
+ * @description This hooks exists to align the bridge with the same account in the alternate wallet when using Wallet Connect on Fuel's side of the bridge
  */
 export function useSyncEthWallets() {
   const { fuel } = useFuel();
+
   const { address: wagmiAddress, status: wagmiStatus } = useWagmiAccount();
   const ethConnectors = useConnectors();
   const ethConnections = useConnections();
@@ -28,22 +30,36 @@ export function useSyncEthWallets() {
     fuelConnector?.name === WalletConnectName;
   const wagmiConnected = wagmiStatus === 'connected';
   const wagmiDisconnected = wagmiStatus === 'disconnected';
-  const invalidWagmiWallet = wagmiDisconnected || !wagmiAddress;
 
-  function disconnectBoth() {
-    fuelDisconnect();
-    ethConnectors.forEach((connector: Connector, _) => {
-      connector.disconnect();
-    });
-  }
+  const activeWalletConnectBridge =
+    wagmiDisconnected ||
+    !wagmiAddress ||
+    !isFuelConnectorEthereumWallets ||
+    !wagmiConnected ||
+    !ethConnections.length;
 
+  // Should disconnect both sides when WalletConnect disconnects
   useEffect(() => {
-    if (
-      invalidWagmiWallet ||
-      !isFuelConnectorEthereumWallets ||
-      !wagmiConnected ||
-      !ethConnections.length
-    ) {
+    const onConnectionChange = (connected: boolean) => {
+      if (!connected) {
+        disconnectBoth();
+      }
+    };
+    if (fuelConnector?.name === WalletConnectName) {
+      fuelConnector?.on(FuelConnectorEventTypes.connection, onConnectionChange);
+
+      return () => {
+        fuelConnector?.off(
+          FuelConnectorEventTypes.connection,
+          onConnectionChange,
+        );
+      };
+    }
+  }, [fuelConnector]);
+
+  // Should use the same wallet and accounts if using WalletConnect
+  useEffect(() => {
+    if (activeWalletConnectBridge) {
       return;
     }
 
@@ -56,10 +72,12 @@ export function useSyncEthWallets() {
       });
       disconnectBoth();
     }
-  }, [
-    invalidWagmiWallet,
-    isFuelConnectorEthereumWallets,
-    wagmiConnected,
-    ethConnections.length,
-  ]);
+  }, [activeWalletConnectBridge, ethConnections.length]);
+
+  function disconnectBoth() {
+    fuelDisconnect();
+    ethConnectors.forEach((connector: Connector, _) => {
+      connector.disconnect();
+    });
+  }
 }
