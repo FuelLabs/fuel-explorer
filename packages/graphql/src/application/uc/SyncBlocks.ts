1c33803bd88c594c6a462ed98ad334c19fb0cb60
Rodrigo Branas <rodrigo.branas@gmail.com>
2024-08-03 17:12:34 -0300

    fix: removing Worker

--- packages/graphql/src/application/uc/SyncBlocks.ts
+++
@@ -1,9 +0,0 @@
-import type { QueueInputs, QueueNames } from '~/infra/queue/Queue';
-import { worker } from '~/infra/worker/Worker';
-
-export type SyncBlocksProps = QueueInputs[QueueNames.SYNC_BLOCKS];
-
-export const syncBlocks = async (data: SyncBlocksProps) => {
-  console.log('🔁 Syncing blocks', data);
-  worker.run(data);
-};
