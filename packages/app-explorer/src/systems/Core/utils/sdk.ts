ae9244805fdfbe03d2785b6383f34a78d0abc327
Luiz Gomes <8636507+LuizAsFight@users.noreply.github.com>
2024-08-14 18:42:00 -0300

    Update sdk.ts

--- packages/app-explorer/src/systems/Core/utils/sdk.ts
+++ packages/app-explorer/src/systems/Core/utils/sdk.ts
@@ -2,9 +2,11 @@ import { getSdk } from '@fuel-explorer/graphql/sdk';
 import { GraphQLClient } from 'graphql-request';
 
 const FUEL_INDEXER_API_KEY = process.env.FUEL_INDEXER_API_KEY;
-const FUEL_INDEXER_API =
-  process.env.FUEL_INDEXER_API ||
-  'https://testnet-indexer-staging.fuel.network/graphql';
+const FUEL_INDEXER_API = process.env.FUEL_INDEXER_API;
+
+if (!FUEL_INDEXER_API) {
+  throw new Error('Needs to inform env variable<FUEL_INDEXER_API> to Fuel Indexer API.');
+}
 
 const getHeaders = () => {
   if (FUEL_INDEXER_API_KEY) {
