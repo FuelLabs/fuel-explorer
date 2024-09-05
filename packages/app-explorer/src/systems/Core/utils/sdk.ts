0994ae9c382e3bb5118e108d1ebcddd5c1e32bbc
Rodrigo Branas <rodrigo.branas@gmail.com>
2024-09-05 15:19:02 -0300

    chore: mainnet security api key (#470)

--- packages/app-explorer/src/systems/Core/utils/sdk.ts
+++ packages/app-explorer/src/systems/Core/utils/sdk.ts
@@ -3,6 +3,7 @@ import { GraphQLClient } from 'graphql-request';
 
 const FUEL_INDEXER_API_KEY = process.env.FUEL_INDEXER_API_KEY;
 const FUEL_INDEXER_API = process.env.FUEL_INDEXER_API;
+const FUEL_INDEXER_MAINNET_KEY = process.env.FUEL_INDEXER_MAINNET_KEY;
 
 if (!FUEL_INDEXER_API) {
   throw new Error(
@@ -11,10 +12,16 @@ if (!FUEL_INDEXER_API) {
 }
 
 const getHeaders = () => {
+  const headers: any = {};
   if (FUEL_INDEXER_API_KEY) {
-    return { 'x-api-key': `Bearer ${FUEL_INDEXER_API_KEY}` };
+    headers['x-api-key'] = `Bearer ${FUEL_INDEXER_API_KEY}`;
+    headers.Authorization = `Bearer ${FUEL_INDEXER_API_KEY}`;
   }
-  return undefined;
+  // TODO: remove after mainnet endpoint become public
+  if (FUEL_INDEXER_MAINNET_KEY) {
+    headers.Authorization = `Basic ${FUEL_INDEXER_MAINNET_KEY}`;
+  }
+  return headers;
 };
 
 const client = new GraphQLClient(FUEL_INDEXER_API, {
