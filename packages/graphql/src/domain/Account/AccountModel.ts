6ea98873fd79bf3e6d07bfe8041b58311397cd78
raghukapur9 <64493087+raghukapur9@users.noreply.github.com>
2024-09-13 16:03:33 -0400

    removed AccountModel class

--- packages/graphql/src/domain/Account/AccountModel.ts
+++
@@ -1,9 +0,0 @@
-import { integer, jsonb, pgTable, serial, text } from 'drizzle-orm/pg-core';
-
-// Define the schema for the accounts table
-export const AccountsTable = pgTable('accounts', {
-  _id: serial('_id').primaryKey(),
-  account_id: text('account_id').notNull().unique(),
-  transaction_count: integer('transaction_count').notNull().default(0),
-  data: jsonb('data').notNull().default({}),
-});
