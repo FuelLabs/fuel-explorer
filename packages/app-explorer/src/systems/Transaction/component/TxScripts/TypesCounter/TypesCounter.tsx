a5d46a9924e6e74e63fda6a791359b8c64a0cad9
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-09-13 16:41:02 -0300

    refactor: types counter from filters to a single reduce

--- packages/app-explorer/src/systems/Transaction/component/TxScripts/TypesCounter/TypesCounter.tsx
+++ packages/app-explorer/src/systems/Transaction/component/TxScripts/TypesCounter/TypesCounter.tsx
@@ -1,59 +1,82 @@
 import { GQLReceiptType } from '@fuel-explorer/graphql/sdk';
+import { useMemo } from 'react';
 import { CountReceipt } from '~/systems/Transaction/component/TxScripts/CountReceipt';
 import type { TypesCounterProps } from './types';
 
-export function TypesCounter({ receipts: items = [] }: TypesCounterProps) {
-  const receipts = items ?? [];
-  const calls = receipts.filter((i) => i?.receiptType === GQLReceiptType.Call);
-  const transfers = receipts.filter(
-    (i) =>
-      i?.receiptType === GQLReceiptType.Transfer ||
-      i?.receiptType === GQLReceiptType.TransferOut,
+const EMPTY_ARRAY = [] as NonNullable<TypesCounterProps['receipts']>;
+
+export function TypesCounter({ receipts: items }: TypesCounterProps) {
+  const counts = useMemo(
+    () =>
+      (items ?? EMPTY_ARRAY).reduce(
+        (acc, receipt) => {
+          switch (receipt?.receiptType) {
+            case GQLReceiptType.Call:
+              acc.calls += 1;
+              break;
+            case GQLReceiptType.Transfer:
+            case GQLReceiptType.TransferOut:
+              acc.transfers += 1;
+              break;
+            case GQLReceiptType.Mint:
+              acc.mints += 1;
+              break;
+            case GQLReceiptType.Burn:
+              acc.burns += 1;
+              break;
+            case GQLReceiptType.MessageOut:
+              acc.messages += 1;
+              break;
+            case GQLReceiptType.Return:
+            case GQLReceiptType.ReturnData:
+              acc.returns += 1;
+              break;
+            case GQLReceiptType.ScriptResult:
+              acc.results += 1;
+              break;
+            case GQLReceiptType.Panic:
+            case GQLReceiptType.Revert:
+              acc.errors += 1;
+              break;
+            case GQLReceiptType.Log:
+            case GQLReceiptType.LogData:
+              acc.logs += 1;
+              break;
+            default:
+              break;
+          }
+          return acc;
+        },
+        {
+          calls: 0,
+          transfers: 0,
+          mints: 0,
+          burns: 0,
+          messages: 0,
+          returns: 0,
+          results: 0,
+          errors: 0,
+          logs: 0,
+        },
+      ),
+    [items],
   );
-  const mints = receipts.filter((i) => i?.receiptType === GQLReceiptType.Mint);
-  const burns = receipts.filter((i) => i?.receiptType === GQLReceiptType.Burn);
-  const messages = receipts.filter(
-    (i) => i?.receiptType === GQLReceiptType.MessageOut,
-  );
-  const returns = receipts.filter(
-    (i) =>
-      i?.receiptType === GQLReceiptType.Return ||
-      i?.receiptType === GQLReceiptType.ReturnData,
-  );
-  const results = receipts.filter(
-    (i) => i?.receiptType === GQLReceiptType.ScriptResult,
-  );
-  const errors = receipts.filter(
-    (i) =>
-      i?.receiptType === GQLReceiptType.Panic ||
-      i?.receiptType === GQLReceiptType.Revert,
-  );
-  const logs = receipts.filter(
-    (i) =>
-      i?.receiptType === GQLReceiptType.Log ||
-      i?.receiptType === GQLReceiptType.LogData,
-  );
+
   return (
     <div className="flex flex-col gap-0 text-sm font-mono w-full">
-      {Boolean(calls.length) && <CountReceipt num={calls.length} op="Call" />}
-      {Boolean(logs.length) && <CountReceipt num={logs.length} op="Log" />}
-      {Boolean(transfers.length) && (
-        <CountReceipt num={transfers.length} op="Transfer" />
+      {counts.calls > 0 && <CountReceipt num={counts.calls} op="Call" />}
+      {counts.logs > 0 && <CountReceipt num={counts.logs} op="Log" />}
+      {counts.transfers > 0 && (
+        <CountReceipt num={counts.transfers} op="Transfer" />
       )}
-      {Boolean(messages.length) && (
-        <CountReceipt num={messages.length} op="Message" />
+      {counts.messages > 0 && (
+        <CountReceipt num={counts.messages} op="Message" />
       )}
-      {Boolean(mints.length) && <CountReceipt num={mints.length} op="Mint" />}
-      {Boolean(burns.length) && <CountReceipt num={burns.length} op="Burn" />}
-      {Boolean(returns.length) && (
-        <CountReceipt num={returns.length} op="Return" />
-      )}
-      {Boolean(results.length) && (
-        <CountReceipt num={results.length} op="Result" />
-      )}
-      {Boolean(errors.length) && (
-        <CountReceipt num={errors.length} op="Error" />
-      )}
+      {counts.mints > 0 && <CountReceipt num={counts.mints} op="Mint" />}
+      {counts.burns > 0 && <CountReceipt num={counts.burns} op="Burn" />}
+      {counts.returns > 0 && <CountReceipt num={counts.returns} op="Return" />}
+      {counts.results > 0 && <CountReceipt num={counts.results} op="Result" />}
+      {counts.errors > 0 && <CountReceipt num={counts.errors} op="Error" />}
     </div>
   );
 }
