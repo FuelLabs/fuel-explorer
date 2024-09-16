53815a9253f848f6a110d67416c0fb9401b6c41b
Arthur Geron <3487334+arthurgeron@users.noreply.github.com>
2024-09-16 12:37:30 -0300

    feat: improve block data display on different breakpoints

--- packages/app-explorer/src/systems/Block/components/BlockScreenSimple.tsx
+++ packages/app-explorer/src/systems/Block/components/BlockScreenSimple.tsx
@@ -31,7 +31,7 @@ export function BlockScreenSimple({
 }: BlockScreenSimpleProps) {
   return (
     <VStack>
-      <Grid className="grid-rows-3 tablet:grid-rows-1 tablet:grid-cols-3 gap-6 mb-8">
+      <Grid className="grid-rows-3 tablet:grid-rows-2 tablet:grid-cols-2 desktop:grid-cols-4 gap-6 mb-8">
         <CardInfo name="Producer" className="flex-1">
           <Address
             value={producer || ''}
