89c250dbd02857dcf72497ffbb1bf03d2327c866
Ays-lab <aysel_8806@hotmail.com>
2024-09-16 23:28:58 +0300

    Update block-commiter.sh

--- docker/block-committer/block-commiter.sh
+++ docker/block-committer/block-commiter.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
-set -euo
+set -euo pipefail
 
+# Define default values for retries and delay
 RETRIES=${RETRIES:-60}
 DELAY=${DELAY:-10}
-JSON='{"jsonrpc":"2.0","id":0,"method":"net_version","params":[]}'
+JSON='{"jsonrpc":"2.0","id":0,"method":"net_version","params":[]}' # JSON payload example
 
-if [ -z "$ETHEREUM_RPC" ]; then
-    echo "Must specify \$ETHEREUM_RPC."
+# Ensure required environment variables are set
+if [ -z "${ETHEREUM_RPC:-}" ]; then
+    echo "Must specify \`ETHEREUM_RPC\`."
     exit 1
 fi
-if [ -z "$FUEL_GRAPHQL_ENDPOINT" ]; then
-    echo "Must specify \$FUEL_GRAPHQL_ENDPOINT."
+if [ -z "${FUEL_GRAPHQL_ENDPOINT:-}" ]; then
+    echo "Must specify \`FUEL_GRAPHQL_ENDPOINT\`."
     exit 1
 fi
-if [ -z "$DEPLOYMENTS_HTTP" ]; then
-    echo "Must specify \$DEPLOYMENTS_HTTP."
+if [ -z "${DEPLOYMENTS_HTTP:-}" ]; then
+    echo "Must specify \`DEPLOYMENTS_HTTP\`."
     exit 1
 fi
 
-echo $FUEL_GRAPHQL_ENDPOINT/health
+echo "Checking Fuel GraphQL endpoint: $FUEL_GRAPHQL_ENDPOINT/health"
 
-# wait for the base layer to be up
+# Wait for the Fuel Core chain to be up
 echo "Waiting for Fuel Core chain."
 curl \
     --fail \
@@ -29,11 +31,11 @@ curl \
     --retry-connrefused \
     --retry $RETRIES \
     --retry-delay $DELAY \
-    $FUEL_GRAPHQL_ENDPOINT/health > /dev/null
+    "$FUEL_GRAPHQL_ENDPOINT/health" > /dev/null
 echo "Connected to Fuel Core chain."
 
-# get the deployments file from the deployer
-echo "Waiting for l1 chain deployment data."
+# Retrieve the deployments file from the deployer
+echo "Waiting for L1 chain deployment data."
 curl \
     --fail \
     --show-error \
@@ -42,16 +44,16 @@ curl \
     --retry-all-errors \
     --retry $RETRIES \
     --retry-delay $DELAY \
-    $DEPLOYMENTS_HTTP \
+    "$DEPLOYMENTS_HTTP" \
     -o addresses.json
-echo "Got l1 chain deployment data."
+echo "Got L1 chain deployment data."
 
-# pull data from deployer dump
-export STATE_CONTRACT_ADDRESS=$(cat "./addresses.json" | jq -r .FuelChainState)
+# Extract data from the deployments file
+export STATE_CONTRACT_ADDRESS=$(jq -r .FuelChainState < ./addresses.json)
 echo "STATE_CONTRACT_ADDRESS: $STATE_CONTRACT_ADDRESS"
 echo "ETHEREUM_RPC: $ETHEREUM_RPC"
 echo "FUEL_GRAPHQL_ENDPOINT: $FUEL_GRAPHQL_ENDPOINT"
 
-# start the Block Commiter
-echo "Starting block commiter"
+# Start the Block Committer
+echo "Starting block committer"
 exec /root/fuel-block-committer
