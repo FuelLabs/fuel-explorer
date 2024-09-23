615f93459c6c1aafd433c2375f456121070aaaf3
raghukapur9 <64493087+raghukapur9@users.noreply.github.com>
2024-09-23 17:32:51 -0400

    fixed table heading typo

--- packages/app-explorer/src/systems/Block/components/BlocksTable.tsx
+++ packages/app-explorer/src/systems/Block/components/BlocksTable.tsx
@@ -82,7 +82,7 @@ const columns = [
   {
     name: (
       <div className=" group">
-        <span className="">Effieciency</span>
+        <span className="">Efficiency</span>
         <div className="absolute left-[20px] top-[30px] w-[20rem] opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10 px-3 py-2 text-xs font-light text-white bg-gray-3 rounded-lg shadow-sm">
           The percentage of block resources utilized by transactions.
           <div className="absolute left-[10px] top-[-6px] w-0 h-0 border-l-[6px] border-l-transparent border-r-[6px] border-r-transparent border-b-[6px] border-b-gray-3" />
