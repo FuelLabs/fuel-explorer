1b133f5f163638c15945bd8992ba07b9031c57f9
LuizAsFight <felipebolsonigomes@gmail.com>
2024-09-24 19:56:51 -0300

    chore

--- packages/e2e-tests/playwright.config.ts
+++ packages/e2e-tests/playwright.config.ts
@@ -24,6 +24,7 @@ const config: PlaywrightTestConfig = defineConfig({
     port: Number(PORT),
     reuseExistingServer: true,
     cwd: join(__dirname, '../../'),
+    stdout: 'pipe',
   },
   use: {
     baseURL: `http://localhost:${PORT}/`,
