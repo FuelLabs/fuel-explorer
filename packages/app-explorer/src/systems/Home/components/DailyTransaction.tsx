2074eecd8ce51219954edd3a95596d11bb275b87
ankit723 <varanasiartist.omg@gmail.com>
2024-09-25 00:45:06 +0530

    rebase

--- packages/app-explorer/src/systems/Home/components/DailyTransaction.tsx
+++ packages/app-explorer/src/systems/Home/components/DailyTransaction.tsx
@@ -42,13 +42,9 @@ const DailyTransaction = (blocks: DailyTransactionProps) => {
     <RoundedContainer className="py-4 px-5 space-y-8 ">
       <div className="space-y-[16px]">
         <div className="flex items-center justify-between">
-          <div className="text-[15px] leading-[24px] text-heading font-semibold group">
-            <span className="">Daily Transactions</span>
-            <div className="absolute left-[20px] top-[30px] w-[20rem] opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10 px-3 py-2 text-xs font-light text-black dark:text-white bg-gray-3 rounded-lg shadow-sm">
-              The percentage of block resources utilized by transactions.
-              <div className="absolute left-[10px] top-[-6px] w-0 h-0 border-l-[6px] border-l-transparent border-r-[6px] border-r-transparent border-b-[6px] border-b-gray-3" />
-            </div>
-          </div>
+          <h3 className="text-[15px] leading-[24px] text-heading font-semibold">
+            Daily Transactions
+          </h3>
           <span className="text-[13px] leading-[20px] text-muted block">
             24H
           </span>
