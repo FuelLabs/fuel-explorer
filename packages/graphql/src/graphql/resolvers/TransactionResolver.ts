aaa22c1cc0a2ac21329240a8e472d048decf8f40
raghukapur9 <raghukapur9@gmail.com>
2024-09-25 01:33:09 -0400

    transaction statistics

--- packages/graphql/src/graphql/resolvers/TransactionResolver.ts
+++ packages/graphql/src/graphql/resolvers/TransactionResolver.ts
@@ -1,8 +1,12 @@
 import type {
+  GQLQueryCumulativeFeeStatisticsArgs,
+  GQLQueryCumulativeTransactionStatisticsArgs,
   GQLQueryTransactionArgs,
+  GQLQueryTransactionFeeStatisticsArgs,
   GQLQueryTransactionsArgs,
   GQLQueryTransactionsByBlockIdArgs,
   GQLQueryTransactionsByOwnerArgs,
+  GQLQueryTransactionsStatisticsArgs,
   GQLTransaction,
 } from '~/graphql/generated/sdk-provider';
 import TransactionDAO from '~/infra/dao/TransactionDAO';
@@ -14,6 +18,10 @@ type Params = {
   transactions: GQLQueryTransactionsArgs;
   transactionByOwner: GQLQueryTransactionsByOwnerArgs;
   transactionByBlockId: GQLQueryTransactionsByBlockIdArgs;
+  transactionsStatistics: GQLQueryTransactionsStatisticsArgs;
+  transactionFeeStatistics: GQLQueryTransactionFeeStatisticsArgs;
+  cumulativeFeeStatistics: GQLQueryCumulativeFeeStatisticsArgs;
+  cumulativeTransactionStatistics: GQLQueryCumulativeTransactionStatisticsArgs;
 };
 
 export class TransactionResolver {
@@ -25,6 +33,11 @@ export class TransactionResolver {
         transactions: resolvers.transactions,
         transactionsByOwner: resolvers.transactionsByOwner,
         transactionsByBlockId: resolvers.transactionsByBlockId,
+        transactionsStatistics: resolvers.getTransactionsStatistics,
+        transactionFeeStatistics: resolvers.getTransactionFeeStatistics,
+        cumulativeFeeStatistics: resolvers.getCumulativeFeeStatistics,
+        cumulativeTransactionStatistics:
+          resolvers.getCumulativeTransactionStatistics,
       },
     };
   }
@@ -65,5 +78,56 @@ export class TransactionResolver {
       paginatedParams,
     );
     return transactions;
+  }
+
+  async getTransactionsStatistics(
+    _: Source,
+    _params: Params['transactionsStatistics'],
+  ) {
+    const transactionDAO = new TransactionDAO();
+    const transactionStats = await transactionDAO.getTransactionsStatistics(
+      _params.timeFilter ? _params.timeFilter : '',
+    );
+
+    return transactionStats;
+  }
+
+  async getTransactionFeeStatistics(
+    _: Source,
+    _params: Params['transactionFeeStatistics'],
+  ) {
+    const transactionDAO = new TransactionDAO();
+    const transactionFeeStats =
+      await transactionDAO.getTransactionFeeStatistics(
+        _params.timeFilter ? _params.timeFilter : '',
+      );
+
+    return transactionFeeStats;
+  }
+
+  async getCumulativeFeeStatistics(
+    _: Source,
+    _params: Params['cumulativeFeeStatistics'],
+  ) {
+    const transactionDAO = new TransactionDAO();
+    const cumulativeFeeStatistics =
+      await transactionDAO.getCumulativeFeeStatistics(
+        _params.timeFilter ? _params.timeFilter : '',
+      );
+
+    return cumulativeFeeStatistics;
+  }
+
+  async getCumulativeTransactionStatistics(
+    _: Source,
+    _params: Params['cumulativeTransactionStatistics'],
+  ) {
+    const transactionDAO = new TransactionDAO();
+    const cumulativeTransactionStatistics =
+      await transactionDAO.getCumulativeTransactionStatistics(
+        _params.timeFilter ? _params.timeFilter : '',
+      );
+
+    return cumulativeTransactionStatistics;
   }
 }
--- packages/graphql/src/infra/dao/TransactionDAO.ts
+++ packages/graphql/src/infra/dao/TransactionDAO.ts
@@ -2,6 +2,7 @@ import { isB256, isBech32 } from 'fuels';
 import { TransactionEntity } from '~/domain/Transaction/TransactionEntity';
 import { DatabaseConnection } from '../database/DatabaseConnection';
 import PaginatedParams from '../paginator/PaginatedParams';
+import { getTimeInterval } from './utils';
 
 export default class TransactionDAO {
   databaseConnection: DatabaseConnection;
@@ -293,5 +294,256 @@ export default class TransactionDAO {
     };
 
     return paginatedResults;
+  }
+
+  async findLatestStatistics() {
+    const [result] = await this.databaseConnection.query(
+      `
+        SELECT * FROM indexer.transaction_statistics
+        ORDER BY timestamp DESC
+        LIMIT 1
+      `,
+      [],
+    );
+    return result;
+  }
+
+  async getEarliestTransactionTimestamp(): Promise<string> {
+    const [result] = await this.databaseConnection.query(
+      `
+      SELECT timestamp
+      FROM indexer.transactions
+      ORDER BY timestamp ASC
+      LIMIT 1
+      `,
+      [],
+    );
+    return result ? result.timestamp : null;
+  }
+
+  async getTransactionsInRange(startTimestamp: string, endTimestamp: string) {
+    const transactionsData = await this.databaseConnection.query(
+      `
+        SELECT
+          (data->'status'->>'totalFee')::numeric AS fee,
+          (data->'status'->>'totalGas')::numeric AS gas,
+          block_id
+        FROM 
+          indexer.transactions
+        WHERE 
+          timestamp >= $1  AND timestamp < $2
+        ORDER BY block_id ASC
+      `,
+      [startTimestamp, endTimestamp],
+    );
+    return transactionsData;
+  }
+
+  async insertTransactionStatistics(
+    timestamp: string,
+    start_block: number,
+    end_block: number,
+    transaction_count: number,
+    transaction_count_cumulative: number,
+    gas_used: number,
+    gas_used_cumulative: number,
+    fee_spent: number,
+    fee_spent_cumulative: number,
+  ) {
+    await this.databaseConnection.query(
+      `
+        INSERT INTO indexer.transaction_statistics (timestamp, start_block, end_block, transaction_count, transaction_count_cumulative, gas_used, gas_used_cumulative, fee_spent, fee_spent_cumulative)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+      `,
+      [
+        timestamp,
+        start_block,
+        end_block,
+        transaction_count,
+        transaction_count_cumulative,
+        gas_used,
+        gas_used_cumulative,
+        fee_spent,
+        fee_spent_cumulative,
+      ],
+    );
+  }
+
+  async getTransactionsStatistics(timeFilter: string) {
+    const _hours = getTimeInterval(timeFilter);
+    let query;
+    if (timeFilter === '1day') {
+      query = `
+        SELECT 
+          to_char(t.timestamp, 'dd/mm/yyyy HH') as time,
+          sum(t.transaction_count::numeric) as tx_count 
+        FROM 
+          indexer.transaction_statistics t
+      `;
+    } else {
+      query = `
+        SELECT 
+          to_char(t.timestamp, 'dd/mm/yyyy') as time,
+          sum(t.transaction_count::numeric) as tx_count 
+        FROM 
+          indexer.transaction_statistics t
+      `;
+    }
+
+    if (_hours !== null) {
+      query += `
+        WHERE t.timestamp > (now() - interval '${_hours} hours')
+      `;
+    }
+    if (timeFilter === '1day') {
+      query += `
+        GROUP BY to_char(t.timestamp, 'dd/mm/yyyy HH');
+      `;
+    } else {
+      query += `
+        GROUP BY to_char(t.timestamp, 'dd/mm/yyyy');
+      `;
+    }
+    const result = await this.databaseConnection.query(query, []);
+    return {
+      nodes: result,
+    };
+  }
+
+  async getTransactionFeeStatistics(timeFilter: string) {
+    const _hours = getTimeInterval(timeFilter);
+    let query;
+    if (timeFilter === '1day') {
+      query = `
+        SELECT
+          to_char(t.timestamp, 'dd/mm/yyyy HH') as time,
+          sum(t.transaction_count::numeric) as tx_count,
+          sum(t.fee_spent::numeric) as fee_spent 
+        FROM 
+          indexer.transaction_statistics t
+      `;
+    } else {
+      query = `
+        SELECT 
+          to_char(t.timestamp, 'dd/mm/yyyy') as time,
+          sum(t.transaction_count::numeric) as tx_count,
+          sum(t.fee_spent::numeric) as fee_spent 
+        FROM 
+          indexer.transaction_statistics t
+      `;
+    }
+
+    if (_hours !== null) {
+      query += `
+        WHERE t.timestamp > (now() - interval '${_hours} hours')
+      `;
+    }
+    if (timeFilter === '1day') {
+      query += `
+        GROUP BY to_char(t.timestamp, 'dd/mm/yyyy HH');
+      `;
+    } else {
+      query += `
+        GROUP BY to_char(t.timestamp, 'dd/mm/yyyy');
+      `;
+    }
+    const result = await this.databaseConnection.query(query, []);
+    return {
+      nodes: result,
+    };
+  }
+
+  async getCumulativeFeeStatistics(timeFilter: string) {
+    const _hours = getTimeInterval(timeFilter);
+    let query;
+
+    if (timeFilter === '1day') {
+      query = `
+        WITH ranked_transactions AS (
+          SELECT
+            to_char(t.timestamp, 'dd/mm/yyyy HH') as time,
+            t.fee_spent_cumulative,
+            ROW_NUMBER() OVER (PARTITION BY to_char(t.timestamp, 'dd/mm/yyyy HH') ORDER BY t.timestamp DESC) as row_num
+          FROM 
+            indexer.transaction_statistics t
+        `;
+    } else {
+      query = `
+        WITH ranked_transactions AS (
+          SELECT
+            to_char(t.timestamp, 'dd/mm/yyyy') as time,
+            t.fee_spent_cumulative,
+            ROW_NUMBER() OVER (PARTITION BY to_char(t.timestamp, 'dd/mm/yyyy') ORDER BY t.timestamp DESC) as row_num
+          FROM 
+            indexer.transaction_statistics t
+        `;
+    }
+
+    if (_hours !== null) {
+      query += `
+          WHERE t.timestamp > (now() - interval '${_hours} hours')
+        `;
+    }
+
+    query += `
+        )
+        SELECT
+          time,
+          fee_spent_cumulative as fee_spent_cumulative
+        FROM ranked_transactions
+        WHERE row_num = 1
+    `;
+
+    const result = await this.databaseConnection.query(query, []);
+    return {
+      nodes: result,
+    };
+  }
+
+  async getCumulativeTransactionStatistics(timeFilter: string) {
+    const _hours = getTimeInterval(timeFilter);
+    let query;
+
+    if (timeFilter === '1day') {
+      query = `
+        WITH ranked_transactions AS (
+          SELECT
+            to_char(t.timestamp, 'dd/mm/yyyy HH') as time,
+            t.transaction_count_cumulative,
+            ROW_NUMBER() OVER (PARTITION BY to_char(t.timestamp, 'dd/mm/yyyy HH') ORDER BY t.timestamp DESC) as row_num
+          FROM 
+            indexer.transaction_statistics t
+        `;
+    } else {
+      query = `
+        WITH ranked_transactions AS (
+          SELECT
+            to_char(t.timestamp, 'dd/mm/yyyy') as time,
+            t.transaction_count_cumulative,
+            ROW_NUMBER() OVER (PARTITION BY to_char(t.timestamp, 'dd/mm/yyyy') ORDER BY t.timestamp DESC) as row_num
+          FROM 
+            indexer.transaction_statistics t
+        `;
+    }
+
+    if (_hours !== null) {
+      query += `
+          WHERE t.timestamp > (now() - interval '${_hours} hours')
+        `;
+    }
+
+    query += `
+        )
+        SELECT
+          time,
+          transaction_count_cumulative
+        FROM ranked_transactions
+        WHERE row_num = 1
+    `;
+
+    const result = await this.databaseConnection.query(query, []);
+    return {
+      nodes: result,
+    };
   }
 }
