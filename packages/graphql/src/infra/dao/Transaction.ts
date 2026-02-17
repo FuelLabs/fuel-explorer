224ab8a9f298819699d7f73a7ddb8817038c3f58
Nelito Junior <ngvjqp@gmail.com>
2026-02-17 11:57:15 -0300

    Update transaction timestamp handling to default to current date if status time is unavailable

--- packages/graphql/src/infra/dao/Transaction.ts
+++ packages/graphql/src/infra/dao/Transaction.ts
@@ -12,7 +12,9 @@ export default class Transaction {
     const indexStr = (index + 1).toString().padStart(16, '0');
     this.id = `${height}-${indexStr}`;
     this.transactionHash = data.id;
-    this.timestamp = DateHelper.tai64toDate(data.status.time).toDate();
+    this.timestamp = data.status?.time
+      ? DateHelper.tai64toDate(data.status.time).toDate()
+      : new Date();
     this.data = data;
     this.blockId = blockId;
   }
