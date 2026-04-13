74e51af1acbd3b1e86d18a92f07b31071e30a036
Nelito Junior <ngvjqp@gmail.com>
2026-04-13 11:21:58 -0300

    Add MaintenanceBanner and include in Layout
    Add a new MaintenanceBanner component that displays an orange Alert with an alert icon and a message about a scheduled network migration. Import and render the banner at the top of the Core Layout so the site shows a site-wide maintenance notice across pages.

---
+++ packages/app-explorer/src/systems/Core/components/MaintenanceBanner/MaintenanceBanner.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { Alert, Text } from '@fuels/ui';
+import { IconAlertTriangle } from '@tabler/icons-react';
+
+export const MaintenanceBanner = () => {
+  return (
+    <Alert
+      color="orange"
+      size="1"
+      variant="surface"
+      className="rounded-none justify-center"
+    >
+      <Alert.Icon>
+        <IconAlertTriangle className="text-orange-12" />
+      </Alert.Icon>
+      <Text className="text-orange-12">
+        The network is undergoing a scheduled migration today starting 17:30
+        UTC. We expect to be back up shortly.
+      </Text>
+    </Alert>
+  );
+};
