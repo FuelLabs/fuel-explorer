ae3568a6fd5847e6b96ee6e252f5affc9c94d114
Nelito Junior <ngvjqp@gmail.com>
2026-08-29 08:05:54 -0300

    chore: remove the Postgres API and syncer, serve the explorer from api-lite
    - delete the graphql package's server, syncer, cosmos worker, pm2 configs, Postgres DAOs, migrations and tests; keep schemas, generated SDK, domain layer and value objects as a library
    - delete the helm chart, cluster/minikube scripts, Tiltfile, deployment/Dockerfile, the docker-publish and helm-publish workflows and the run/setup backend scripts
    - drop the Postgres service and indexer env from docker/docker-compose.yml
    - graphql package dependencies 36 to 9, devDependencies 31 to 21

--- packages/graphql/src/domain/Block/vo/BlockProducer.ts
+++ packages/graphql/src/domain/Block/vo/BlockProducer.ts
@@ -1,8 +1,6 @@
 import { varchar } from 'drizzle-orm/pg-core';
-import { Signer } from 'fuels';
 import { Address } from '~/core/Address';
 import { ValueObject } from '~/core/ValueObject';
-import { client } from '~/graphql/GraphQLSDK';
 
 interface Props {
   value: Address | null;
@@ -17,18 +15,6 @@ export class BlockProducer extends ValueObject<Props> {
     if (!id) return new BlockProducer({ value: null });
     const address = new Address(id);
     return new BlockProducer({ value: address });
-  }
-
-  static async fromSdk() {
-    // TODO: get from database
-    const blocks = await client.sdk.blocks({ first: 2 });
-    const block = blocks.data.blocks.nodes[0];
-    if (block.consensus.__typename === 'Genesis') {
-      return null;
-    }
-    const signature = block.consensus.signature;
-    const producer = Signer.recoverAddress(block.id, signature).toString();
-    return producer;
   }
 
   value() {
--- packages/graphql/src/infra/timer/Timer.ts
+++
@@ -1,8 +0,0 @@
-export default class Timer {
-  static async register(interval: number, callback: Function) {
-    await callback();
-    setInterval(async () => {
-      await callback();
-    }, interval);
-  }
-}
