d4421d81bccb3599a3e7028af043886c9e940ad1
Nelito Junior <ngvjqp@gmail.com>
2026-09-15 20:48:25 -0300

    chore(explorer): drop Vercel Web Analytics, the script only exists on Vercel

--- packages/app-explorer/src/main.tsx
+++ packages/app-explorer/src/main.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Analytics } from '@vercel/analytics/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
@@ -58,7 +57,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
                   <ErrorBoundary>
                     <App />
                   </ErrorBoundary>
-                  <Analytics />
                 </BrowserRouter>
               </FuelConnectProviderWithTheme>
             </ConnectProvider>
